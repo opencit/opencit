13e7a55f0fe25b7a8b3ecfd25af60b2d9da9d202
Jonathan Buhacoff <jonathan.buhacoff@intel.com>
2013-02-15 19:07:54 -0800

    changing default ssl policy to trust first certificate

--- integration/api-client-jar/src/main/java/com/intel/mtwilson/client/cmd/RegisterUser.java
+++ integration/api-client-jar/src/main/java/com/intel/mtwilson/client/cmd/RegisterUser.java
@@ -90,9 +90,12 @@ public class RegisterUser extends AbstractCommand {
             
             RsaCredentialX509 rsaCredential = keystore.getRsaCredentialX509(username, password);
             Properties p = new Properties();
+            /*
             p.setProperty("mtwilson.api.ssl.policy", "TRUST_CA_VERIFY_HOSTNAME"); // must be secure out of the box!
             p.setProperty("mtwilson.api.ssl.requireTrustedCertificate", "true");
             p.setProperty("mtwilson.api.ssl.verifyHostname", "true");            
+            */
+            p.setProperty("mtwilson.api.ssl.policy", "TRUST_FIRST_CERTIFICATE"); // must be secure out of the box!
             ApiClient c = new ApiClient(server, rsaCredential, keystore, new MapConfiguration(p)); //ConfigurationFactory.fromSystemEnvironment());
             ApiClientCreateRequest user = new ApiClientCreateRequest();
             user.setCertificate(rsaCredential.getCertificate().getEncoded());
