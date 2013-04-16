c31b960aabf46fb499e3fc505b5995bba180a89d
Jim Leier <james.f.leier@intel.com>
2013-04-16 13:25:04 -0700

    work on postgres functions

--- installers/mtwilson-server/src/files/setup.sh
+++ installers/mtwilson-server/src/files/setup.sh
@@ -190,10 +190,12 @@ elif using_postgres; then
     fi 
     postgres_server_install 
     postgres_restart & >> $INSTALL_LOG_FILE
+    sleep 10
   fi
   echo "Installing postgres client..."
   postgres_install
   postgres_restart & >> $INSTALL_LOG_FILE
+  sleep 10
   echo "Installation of postgres client complete..."
 
   postgres_create_database
