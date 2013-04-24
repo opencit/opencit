051c79be5c9e3e76cc021225d88427b1c4e1b70e
Jonathan Buhacoff <jonathan.buhacoff@intel.com>
2013-04-24 08:37:29 -0700

    creating new tool mtwilson-setup, to replace setup-console usage in the installer;  cluster setup will be handled by future project mtwilson-setup-remote or mtwilson-setup-cluster

---
+++ desktop/mtwilson-setup/src/main/java/com/intel/mtwilson/setup/cmd/BootstrapUser.java
@@ -0,0 +1,208 @@
+/*
+ * Copyright (C) 2012 Intel Corporation
+ * All rights reserved.
+ */
+package com.intel.mtwilson.setup.cmd;
+
+import com.intel.mtwilson.KeystoreUtil;
+import com.intel.mtwilson.as.controller.MwKeystoreJpaController;
+import com.intel.mtwilson.as.data.MwKeystore;
+import com.intel.mtwilson.crypto.RsaCredentialX509;
+import com.intel.mtwilson.crypto.SimpleKeystore;
+import com.intel.mtwilson.datatypes.Role;
+import com.intel.mtwilson.io.ByteArrayResource;
+import com.intel.mtwilson.ms.common.MSConfig;
+import com.intel.mtwilson.ms.controller.ApiClientX509JpaController;
+import com.intel.mtwilson.ms.controller.MwPortalUserJpaController;
+import com.intel.mtwilson.ms.data.ApiClientX509;
+import com.intel.mtwilson.ms.data.MwPortalUser;
+import com.intel.mtwilson.ms.helper.MSPersistenceManager;
+import com.intel.dcg.console.Command;
+import com.intel.mtwilson.setup.SetupContext;
+import com.intel.mtwilson.setup.SetupException;
+import com.intel.mtwilson.setup.SetupWizard;
+import com.intel.mtwilson.setup.helper.SCPersistenceManager;
+import java.io.BufferedReader;
+import java.io.Console;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.URL;
+import java.net.UnknownHostException;
+import org.apache.commons.codec.binary.Hex;
+import org.apache.commons.configuration.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author jbuhacoff
+ */
+public class BootstrapUser implements Command {
+    private SCPersistenceManager scManager = new SCPersistenceManager();
+    private MwPortalUserJpaController keystoreJpa = new MwPortalUserJpaController(scManager.getEntityManagerFactory("MSDataPU"));
+    private static final Logger logger = LoggerFactory.getLogger(BootstrapUser.class.getName());
+    private SetupContext ctx = null;
+    public static final Console console = System.console();
+    
+//    @Override
+    public void setContext(SetupContext ctx) {
+        this.ctx = ctx;
+    }
+    
+    private Configuration options = null;
+    @Override
+    public void setOptions(Configuration options) {
+        this.options = options;
+    }
+
+    
+    /**
+     * Creates a new API Client in current directory, registers it with Mt Wilson (on localhost or as configured), and then checks the database for the expected record to validate that it's being created.
+     * @param args
+     * @throws Exception 
+     */
+    @Override
+    public void execute(String[] args) throws Exception {
+        Configuration serviceConf = MSConfig.getConfiguration();
+        
+        //File directory;
+        //String directoryPath = options.getString("keystore.users.dir", "/var/opt/intel/management-console/users"); //serviceConf.getString("mtwilson.mc.keystore.dir", "/var/opt/intel/management-console/users");
+        //if( directoryPath == null || directoryPath.isEmpty() ) {
+        //    directory = new File(directoryPath);
+        //    if( !directory.exists() || !directory.isDirectory() ) {
+        //        directory = new File(".");
+        //    }
+        //    directoryPath = readInputStringWithPromptAndDefault("Keystore directory", directory.getAbsolutePath());
+        //}
+        //directory = new File(directoryPath);
+        
+        String baseurl = options.getString("mtwilson.api.baseurl");
+        if( baseurl == null || baseurl.isEmpty() ) { 
+            baseurl = firstNonEmpty(new String[] { serviceConf.getString("mtwilson.api.baseurl"), System.getenv("MTWILSON_API_BASEURL"), "https://"+getLocalHostAddress()+":8181" }); 
+            baseurl = readInputStringWithPromptAndDefault("Mt Wilson URL", baseurl);
+        }
+        
+        String username = null;
+        String password = null;
+        if( args.length > 0 ) { username = args[0]; } else { username = readInputStringWithPrompt("Username"); }
+        if( args.length > 1 ) { password = args[1]; } else { password = readInputStringWithPrompt("Password"); }
+        if( password != null && password.startsWith("env:") && password.length() > 4 ) {
+            password = System.getenv(password.substring(4)); 
+        }
+        if( password ==  null || password.isEmpty() ) {
+            System.out.println("Password is required");
+            return;
+        }
+        
+        MwPortalUser keyTest = keystoreJpa.findMwPortalUserByUserName(username);
+        if(keyTest != null) {
+          logger.info("An user already exists with the specified User Name. Please select different User Name.");
+          throw new SetupException("User account with that name already exists ");
+        }
+        
+        // create user
+        System.out.println(String.format("Creating keystore for %s in db and registering user with service at %s", username,baseurl));        
+        /*
+        com.intel.mtwilson.client.TextConsole.main(new String[] { "CreateUser", directory.getAbsolutePath(), username, password });
+        File keystoreFile = new File(directory.getAbsolutePath() + File.separator + Filename.encode(username) + ".jks");
+        if( !keystoreFile.exists() ) {
+            System.out.println("Failed to create keystore "+keystoreFile.getAbsolutePath());
+            return;
+        }
+        
+        // register user
+        System.out.println(String.format("Registering %s with service at %s", username, baseurl));
+        com.intel.mtwilson.client.TextConsole.main(new String[] { "RegisterUser", keystoreFile.getAbsolutePath(), baseurl, "Attestation,Whitelist,Security", password });
+        */
+        // stdalex 1/16 jks2db!disk
+        // load the new key
+         ByteArrayResource certResource = new ByteArrayResource();
+         SimpleKeystore keystore = KeystoreUtil.createUserInResource(certResource, username, password, new URL(baseurl),new String[] { Role.Whitelist.toString(),Role.Attestation.toString(),Role.Security.toString()});
+         MwPortalUser keyTable = new MwPortalUser();
+         keyTable.setUsername(username);
+         keyTable.setKeystore(certResource.toByteArray());
+         keyTable.setStatus("PENDING");
+         keystoreJpa.create(keyTable);
+         RsaCredentialX509 rsaCredentialX509 = keystore.getRsaCredentialX509(username, password);
+        // check database for record
+//        ApiClientBO bo = new ApiClientBO();
+//        ApiClientInfo apiClientRecord = bo.find(rsaCredentialX509.identity());
+//        ApiClientInfo apiClientRecord = findApiClientRecord(serviceConf, rsaCredentialX509.identity());
+//        if( apiClientRecord == null ) {
+        // approve user
+        approveApiClientRecord(serviceConf, rsaCredentialX509.identity());
+        System.out.println(String.format("Approved %s [fingerprint %s]", username, Hex.encodeHexString(rsaCredentialX509.identity())));        
+    }
+    
+    private void approveApiClientRecord(Configuration conf, byte[] fingerprint) throws SetupException {
+        SetupWizard wizard = new SetupWizard(conf);
+        try {
+            // XXX UNTESTED postgres support: instead of using hard-coded query, we use the JPA mechanism here and move the compatibility problem to JPA
+            /*
+            Connection c = wizard.getMSDatabaseConnection();        
+            PreparedStatement s = c.prepareStatement("UPDATE mw_api_client_x509 SET enabled=b'1',status='Approved' WHERE hex(fingerprint)=?"); // XXX TODO should use repository code for this, not hardcoded query, because table names may change between releases or deployments
+            //s.setBytes(1, fingerprint);
+            s.setString(1, Hex.encodeHexString(fingerprint));
+            s.executeUpdate();
+            s.close();
+            c.close();
+            */
+            MSPersistenceManager persistenceManager = new MSPersistenceManager();
+            ApiClientX509JpaController jpaController = new ApiClientX509JpaController(persistenceManager.getEntityManagerFactory("MSDataPU"));
+            ApiClientX509 apiClient = jpaController.findApiClientX509ByFingerprint(fingerprint);
+            apiClient.setStatus("Approved");
+            apiClient.setEnabled(true);
+            jpaController.edit(apiClient);
+        }
+        catch(Exception e) {
+            throw new SetupException("Cannot update API Client record: "+e.getMessage(), e);
+        }        
+    }
+
+    
+    // XXX see also RemoteCommand in com.intel.mtwilson.setup (not used) and com.intel.mtwilson (in api-client)
+    private static String getLocalHostAddress() {
+        try {
+            InetAddress addr = InetAddress.getLocalHost();
+            return addr.getHostAddress();
+        } catch (UnknownHostException ex) {
+            return "127.0.0.1";
+        }
+    }
+    
+    private String readInputStringWithPrompt(String prompt) throws IOException {
+        if (console == null) {
+            throw new IOException("no console.");
+        }
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print(String.format("%s: ", prompt));
+        String input = in.readLine();
+//        in.close(); // don't close System.in !!
+        return input;
+    }
+
+    private String readInputStringWithPromptAndDefault(String prompt, String defaultValue) throws IOException {
+        if (console == null) {
+            throw new IOException("no console.");
+        }
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print(String.format("%s [%s]: ", prompt, defaultValue));
+        String input = in.readLine();
+//        in.close(); // don't close System.in !!
+        if( input == null || input.isEmpty() ) {
+            input = defaultValue;
+        }
+        return input;
+    }
+    
+    private String firstNonEmpty(String[] values) {
+        for(String value : values) {
+            if( value != null && !value.isEmpty() ) {
+                return value;
+            }
+        }
+        return null;
+    }
+
+}
---
+++ desktop/mtwilson-setup/src/main/java/com/intel/mtwilson/setup/cmd/InitDatabase.java
@@ -0,0 +1,339 @@
+/*
+ * Copyright (C) 2012 Intel Corporation
+ * All rights reserved.
+ */
+package com.intel.mtwilson.setup.cmd;
+
+import com.intel.mountwilson.as.common.ASConfig;
+import com.intel.mtwilson.io.Classpath;
+import com.intel.mtwilson.jpa.PersistenceManager;
+import com.intel.dcg.console.Command;
+import com.intel.mtwilson.setup.SetupContext;
+import com.intel.mtwilson.setup.SetupException;
+import com.intel.mtwilson.setup.SetupWizard;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javax.sql.DataSource;
+import org.apache.commons.configuration.Configuration;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+/**
+ * Bug #509 create java program to handle database updates and ensure that 
+ * old updates (already executed) are not executed again
+ * 
+ * TODO: List each .sql file that is supposed to be run (index them by changelog date), check for
+ * current state of database before running scripts (via the mw_changelog table) so we know
+ * which scripts to run based on what has already been noted in the mw_changelog.
+ * 
+ * TODO:  consolidate the persistence units into ASDataPU... the MSDataPU can't really be separate from
+ * the ASDataPU because audit logs need to refer to users and to whitelist data...
+ * 
+ * References:
+ * http://stackoverflow.com/questions/1429172/how-do-i-list-the-files-inside-a-jar-file
+ * http://stackoverflow.com/questions/1044194/running-a-sql-script-using-mysql-with-jdbc
+ * @author jbuhacoff
+ */
+public class InitDatabase implements Command {
+    private SetupContext ctx = null;
+
+//    @Override
+    public void setContext(SetupContext ctx) {
+        this.ctx = ctx;
+    }
+
+    private Configuration options = null;
+    @Override
+    public void setOptions(Configuration options) {
+        this.options = options;
+    }
+
+    @Override
+    public void execute(String[] args) throws SetupException {
+        // first arg:  mysql or postgres  (installer detects and invokes this command with that argument)
+        if( args.length < 1 ) {
+            throw new SetupException("Usage: InitializeMysqlDatabase mysql|postgres");
+        }
+        
+        try {
+            initDatabase(args[0]);
+        }
+        catch(Exception e) {
+            throw new SetupException("Cannot setup database: "+e.toString(), e);
+        }
+
+    }
+    
+    public static class ChangelogEntry {
+        public String id;
+        public String applied_at;
+        public String description;
+        public ChangelogEntry() { }
+        public ChangelogEntry(String id, String applied_at, String description) {
+            this.id = id;
+            this.applied_at = applied_at;
+            this.description = description;
+        }
+    }
+    
+    private void initDatabase(String databaseVendor) throws SetupException, IOException, SQLException {
+        
+        Map<Long,Resource> sql = getSql(databaseVendor); //  TODO change to Map<Long,Resource> and then pass it directly to the populator !!!!
+        
+//        Configuration attestationServiceConf = ASConfig.getConfiguration();
+        DataSource ds = getDataSource();
+        Connection c = ds.getConnection();  // username and password should already be set in the datasource
+        List<ChangelogEntry> changelog = getChangelog(c);
+        HashMap<Long,String> presentChanges = new HashMap<Long,String>(); // what is already in the database according to the changelog
+        for(ChangelogEntry entry : changelog) {
+            presentChanges.put(Long.valueOf(entry.id), entry.applied_at);
+        }
+        
+        HashSet<Long> changesToApply = new HashSet<Long>(sql.keySet());
+        changesToApply.removeAll(presentChanges.keySet());
+        
+        if( changesToApply.isEmpty() ) {
+            System.out.println("No database updates available");
+            return;
+        }
+        
+        ArrayList<Long> changesToApplyInOrder = new ArrayList<Long>(changesToApply);
+        Collections.sort(changesToApplyInOrder);
+        
+        ResourceDatabasePopulator rdp = new ResourceDatabasePopulator();
+        // removing unneeded output as user can't choice what updates to apply
+        // XXX-TODO stdalex this should all be log.info
+        //System.out.println("Available database updates:");
+        for(Long id : changesToApplyInOrder) {
+            //System.out.println(String.format("%d %s", id, basename(sql.get(id).getURL())));
+            rdp.addScript(sql.get(id)); // new ClassPathResource("/com/intel/mtwilson/database/mysql/bootstrap.sql")); // must specify full path to resource
+        }
+        
+        rdp.setContinueOnError(true);
+        rdp.setIgnoreFailedDrops(true);
+        rdp.setSeparator(";");
+        rdp.populate(c);
+        
+        c.close();
+    }
+    
+    private String getDatabaseHostname(Connection c) throws SQLException {
+        String hostname = null;
+        Statement s = c.createStatement();
+//            ResultSet rs = s.executeQuery("SELECT @@version"); // output is 1 column (VARCHAR) with content like this: 5.1.63-0ubuntu0.11.10.1
+//            ResultSet rs = s.executeQuery("SELECT version()"); // output is same as for @@version
+        ResultSet rs = s.executeQuery("SELECT @@hostname"); // output is 1 column (VARCHAR) with content like this: mtwilsondev    (hostname of database server)
+        if( rs.next() ) {
+            int columns = rs.getMetaData().getColumnCount();
+            System.out.println("Got "+columns+" columns from datbase server");
+            System.out.println("First column type: "+rs.getMetaData().getColumnTypeName(1));
+            System.out.println("First column: "+rs.getString(1));
+            hostname = rs.getString(1); 
+        }
+        rs.close();
+        s.close();
+         return hostname;
+    }
+    
+    /**
+     * Locates the SQL files for the specified vendor, and reads them to
+     * create a mapping of changelog-date to SQL content. This mapping can
+     * then be used to select which files to execute against an existing
+     * database.
+     * See also iBatis, which we are (very) roughly emulating.
+     * @param databaseVendor
+     * @return 
+     */
+    private Map<Long,Resource> getSql(String databaseVendor) throws SetupException {
+        System.out.println("Scanning for "+databaseVendor+" SQL files");
+        HashMap<Long,Resource> sqlmap = new HashMap<Long,Resource>();
+        try {
+            Resource[] list = listResources(databaseVendor); // each URL like: jar:file:/C:/Users/jbuhacof/workspace/mountwilson-0.5.4/desktop/setup-console/target/setup-console-0.5.4-SNAPSHOT-with-dependencies.jar!/com/intel/mtwilson/database/mysql/20121226000000_remove_created_by_patch_rc3.sql
+            for(Resource resource : list) {
+                URL url = resource.getURL();
+//                InputStream in = url.openStream();
+//                String sql = IOUtils.toString(in, "UTF-8");
+//                IOUtils.closeQuietly(in);
+                Long timestamp = getTimestampFromSqlFilename(basename(url));
+                if( timestamp != null ) {
+                    sqlmap.put(timestamp, resource);
+                }
+                else {
+                    System.err.println("SQL filename is not in recognized format: "+url.toExternalForm());
+                }
+            }
+        }
+        catch(IOException e) {
+            throw new SetupException("Error while scanning for SQL files: "+e.getLocalizedMessage(), e);
+        }
+        return sqlmap;        
+    }
+    
+    private Resource[] listResources(String databaseVendor) throws IOException {
+        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver(getClass().getClassLoader());
+        Resource[] resources = resolver.getResources("classpath:com/intel/mtwilson/database/"+databaseVendor+"/*.sql");
+        return resources; 
+        /*
+        ArrayList<URL> list = new ArrayList<URL>();
+        for(Resource resource : resources) {
+            list.add(resource.getURL());
+        }
+        return list;*/
+    }
+    
+    Pattern pTimestampName = Pattern.compile("^([0-9]+).*");
+    
+    /**
+     * Given a URL, returns the final component filename
+     * 
+     * Example URL: jar:file:/C:/Users/jbuhacof/workspace/mountwilson-0.5.4/desktop/setup-console/target/setup-console-0.5.4-SNAPSHOT-with-dependencies.jar!/com/intel/mtwilson/database/mysql/20121226000000_remove_created_by_patch_rc3.sql
+     * Example output: 20121226000000_remove_created_by_patch_rc3.sql
+     * @param url
+     * @return 
+     */
+    private String basename(URL url) {
+        String[] parts = StringUtils.split(url.toExternalForm(), "/");
+        return parts[parts.length-1];
+    }
+    
+    /**
+     * @param filename without any path like: 20121226000000_remove_created_by_patch_rc3.sql
+     * @return 
+     */
+    private Long getTimestampFromSqlFilename(String filename) {
+        Matcher mTimestampName = pTimestampName.matcher(filename);
+        if( mTimestampName.matches() ) {
+            String timestamp = mTimestampName.group(1); // the timestamp like: 20121226000000
+            return Long.valueOf(timestamp);
+        }
+        return null;
+    }
+    
+    private void printSqlMap(Map<Long,String> sqlmap) {
+        Set<Long> timestampSet = sqlmap.keySet();
+        ArrayList<Long> timestampList = new ArrayList<Long>();
+        timestampList.addAll(timestampSet);
+        Collections.sort(timestampList);
+        for(Long timestamp : timestampList) {
+            System.out.println("File timestamp: "+timestamp);
+        }
+    }
+    
+    /**
+     * 
+     * @return datasource object for mt wilson database, guaranteed non-null
+     * @throws SetupException if the datasource cannot be obtained
+     */
+    private DataSource getDataSource() throws SetupException {
+        // load the sql files and run them
+        //InputStream in = getClass().getResourceAsStream("/bootstrap.sql");
+        try {
+            DataSource ds = PersistenceManager.getPersistenceUnitInfo("ASDataPU", ASConfig.getJpaProperties()).getNonJtaDataSource();
+            if( ds == null ) {
+                throw new SetupException("Cannot load persistence unit info");
+            }
+            return ds;
+        }
+        catch(IOException e) {
+            throw new SetupException("Cannot load persistence unit info", e);
+        }
+        
+    }
+    
+    private List<String> getTableNames(Connection c) throws SQLException {
+        ArrayList<String> list = new ArrayList<String>();
+        Statement s = c.createStatement();
+        ResultSet rs = s.executeQuery("SHOW TABLES");
+        while(rs.next()) {
+            list.add(rs.getString(1));
+        }
+        return list;
+    }
+    
+    private List<ChangelogEntry> getChangelog(Connection c) throws SQLException {
+        ArrayList<ChangelogEntry> list = new ArrayList<ChangelogEntry>();
+        
+        // first determine if we have the new changelog table `mw_changelog`, or the old one `changelog`, or none at all
+        List<String> tableNames = getTableNames(c);
+        boolean hasMwChangelog = false;
+        boolean hasChangelog = false;
+        if( tableNames.contains("mw_changelog") ) {
+            hasMwChangelog = true;
+        }
+        if( tableNames.contains("changelog") ) {
+            hasChangelog = true;
+        }
+        
+        if( !hasChangelog && !hasMwChangelog) {
+            return list; /*  empty list indicates database is not initialized and all scripts need to be executed */ 
+        }
+        
+        String changelogTableName = null;
+        // if we have both changelog tables, copy all records from old changelog to new changelog and then use that
+        if( hasChangelog && hasMwChangelog ) {
+            PreparedStatement check = c.prepareStatement("SELECT APPLIED_AT FROM `mw_changelog` WHERE ID=?");
+            PreparedStatement insert = c.prepareStatement("INSERT INTO `mw_changelog` SET ID=?, APPLIED_AT=?, DESCRIPTION=?");
+            Statement select = c.createStatement();
+            ResultSet rs = select.executeQuery("SELECT ID,APPLIED_AT,DESCRIPTION FROM `changelog`");
+            while(rs.next()) {
+                check.setLong(1, rs.getLong("ID"));
+                ResultSet rsCheck = check.executeQuery();
+                if( rsCheck.next() ) {
+                    // the id is already in the new mw_changelog table
+                }
+                else {
+                    insert.setLong(1, rs.getLong("ID"));
+                    insert.setString(2, rs.getString("APPLIED_AT"));
+                    insert.setString(3, rs.getString("DESCRIPTION"));
+                    insert.executeUpdate();
+                }
+                rsCheck.close();
+            }
+            rs.close();
+            select.close();
+            insert.close();
+            check.close();
+            changelogTableName = "mw_changelog"; 
+        }
+        else if( hasMwChangelog ) {
+            changelogTableName = "mw_changelog";
+        }
+        else if( hasChangelog ) {
+            changelogTableName = "changelog";
+        }
+        
+        Statement s = c.createStatement();
+        ResultSet rs = s.executeQuery(String.format("SELECT ID,APPLIED_AT,DESCRIPTION FROM `%s`", changelogTableName));
+        while(rs.next()) {
+            ChangelogEntry entry = new ChangelogEntry();
+            entry.id = rs.getString("ID");
+            entry.applied_at = rs.getString("APPLIED_AT");
+            entry.description = rs.getString("DESCRIPTION");
+            list.add(entry);
+        }
+        rs.close();
+        s.close();
+        return list;
+    }
+    
+    
+}
