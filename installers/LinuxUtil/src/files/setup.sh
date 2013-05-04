4c6d493ef96ea930879f140dc3bc4c97a67f9daf
Jonathan Buhacoff <jonathan.buhacoff@intel.com>
2013-05-04 10:49:40 -0700

    updated setup.sh to copy the script plugins

--- installers/LinuxUtil/src/files/setup.sh
+++ installers/LinuxUtil/src/files/setup.sh
@@ -3,10 +3,13 @@
 # *** do NOT use TABS for indentation, use SPACES
 # *** TABS will cause errors in some linux distributions
 
-mkdir -p /usr/local/share/mtwilson/util
-chmod 700 /usr/local/share/mtwilson/util
-cp functions /usr/local/share/mtwilson/util
-cp version /usr/local/share/mtwilson/util
+mkdir -p /usr/share/mtwilson/script
+cp -r plugins /usr/share/mtwilson/script
+chmod -R 700 /usr/share/mtwilson/script
+
+#mkdir -p /usr/local/share/mtwilson/util
+#cp functions /usr/local/share/mtwilson/util
+#cp version /usr/local/share/mtwilson/util
 
 mkdir -p /usr/local/bin
 cp mtwilson /usr/local/bin
