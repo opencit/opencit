d8a7c30c64c49fb94423fe6be29714ddf4406503
Jonathan Buhacoff <jonathan.buhacoff@intel.com>
2013-05-04 23:39:03 -0700

    updated junit test;  created stub for autotest tool

--- integration/api-client-jar/src/test/java/mtwilson/api/AddHost.java
+++ integration/api-client-jar/src/test/java/mtwilson/api/AddHost.java
@@ -13,6 +13,7 @@ import com.intel.mtwilson.datatypes.TxtHostRecord;
 import com.intel.mtwilson.datatypes.Vendor;
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.net.URL;
 import java.security.SignatureException;
 import java.util.List;
 import java.util.ArrayList;
@@ -58,6 +59,38 @@ public class AddHost {
         boolean success = My.client().configureWhiteList(gkvHost); 
         assertTrue(success);
     }
+    
+
+    /**
+     * DOESN'T WORK... THE CONNECTION STRING URL IS RECONSTRUCTED AS intel:https://hostname:port
+     * @throws IOException
+     * @throws ApiException
+     * @throws SignatureException
+     * @throws MalformedURLException
+     * @throws Exception 
+     */
+//    @Test(expected=ApiException.class)
+    public void testAddIntelHostWrongPort() throws IOException, ApiException, SignatureException, MalformedURLException, Exception {
+        // find an intel host to add
+        List<ConnectionString> hosts = My.env().getHostConnectionList();
+        List<ConnectionString> intel = getVendorHosts(hosts, Vendor.INTEL);
+        if( intel.isEmpty() ) {
+            throw new IllegalArgumentException("No Intel hosts in your environment");
+        }
+        ConnectionString conn0 = intel.get(0); // get the first one...
+        URL url = new URL(conn0.getConnectionString());
+        ConnectionString conn = new ConnectionString(Vendor.INTEL, "https://"+url.getHost()+":"+(url.getPort()-1)); // modify the connection string to have a port number where nothing is listening (9998 instead of 9999)
+        System.out.println("Adding "+conn);
+        TxtHostRecord gkvHost = new TxtHostRecord();
+        gkvHost.HostName = conn.getManagementServerName();
+        gkvHost.AddOn_Connection_String = conn.getConnectionStringWithPrefix();
+        HostConfigData hostdata = new HostConfigData();
+        hostdata.setRegisterHost(false);
+        hostdata.setTxtHostRecord(gkvHost);
+        boolean success = My.client().configureWhiteList(hostdata);
+        assertTrue(success);
+    }
+    
 
     @Test
     public void testAddIntelHost() throws IOException, ApiException, SignatureException, MalformedURLException, Exception {
