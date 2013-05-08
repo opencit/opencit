a3bb5742874db393be50e4260071c93c2d2688fb
Sudhir S Bangalore <sudhir.s.bangalore@intel.com>
2013-05-08 11:57:04 +0530

    Fixed the bug in the SQL script because of which the Open Source event type was not getting added into the mw_event_type table.

--- database/mysql/src/main/resources/com/intel/mtwilson/database/mysql/20130430154900_patch_mw12_2.sql
+++ database/mysql/src/main/resources/com/intel/mtwilson/database/mysql/20130430154900_patch_mw12_2.sql
@@ -4,6 +4,6 @@
 -- Unlike VMware, open source hypervisors do not support events. So, we are using a generic name.
 
 
-INSERT INTO mw_event_type (`ID`,`Name`, `FieldName`) VALUES (VALUES (5,'OpenSource.EventName','OpenSource');
+INSERT INTO mw_event_type (`ID`,`Name`, `FieldName`) VALUES (5,'OpenSource.EventName','OpenSource');
 
 INSERT INTO `mw_changelog` (`ID`, `APPLIED_AT`, `DESCRIPTION`) VALUES (20130430154900,NOW(),'patch for adding a new entry into the event type table for open source module attestation.');
