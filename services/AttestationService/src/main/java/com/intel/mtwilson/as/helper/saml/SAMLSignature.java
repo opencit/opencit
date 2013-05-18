a7b0e99172070bb03fd9605425000569730a7bdd
Jonathan Buhacoff <jonathan.buhacoff@intel.com>
2013-05-18 15:56:48 -0700

    added support for SAMLPASSWORD env var when saml.keystore.password or saml.key.password is not set

--- services/AttestationService/src/main/java/com/intel/mtwilson/as/helper/saml/SAMLSignature.java
+++ services/AttestationService/src/main/java/com/intel/mtwilson/as/helper/saml/SAMLSignature.java
@@ -102,7 +102,7 @@ public class SAMLSignature
 //            InputStream keystoreInputStream = keystoreResource.getInputStream(); // this obtains it from the database (or whatever resource is provided)
 //            keyStore = KeyStoreUtil.getKeyStore(SAMLSignature.class.getResourceAsStream(config.getString ("keystore")),config.getString ("storepass"));
             try {
-            	keyStore = getKeyStore(keystoreInputStream,config.getString ("saml.keystore.password"));
+            	keyStore = getKeyStore(keystoreInputStream,config.getString("saml.keystore.password",System.getenv("SAMLPASSWORD")));
             }
             finally {
             	if( keystoreInputStream != null ) {
@@ -112,7 +112,7 @@ public class SAMLSignature
             KeyStore.PrivateKeyEntry entry = (KeyStore.PrivateKeyEntry)
                 keyStore.getEntry (config.getString ("saml.key.alias"), 
                     new KeyStore.PasswordProtection 
-                        (config.getString ("saml.key.password").toCharArray ()));
+                        (config.getString ("saml.key.password", System.getenv("SAMLPASSWORD")).toCharArray ()));
             keyPair = new KeyPair (entry.getCertificate ().getPublicKey (), 
                 entry.getPrivateKey ());
 
