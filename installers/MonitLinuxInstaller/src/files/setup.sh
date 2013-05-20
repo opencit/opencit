1a90f181391d9bc78e0dca39e2ef28f9b9ee92e5
Stewart Dale <stewartx.t.dale@intel.com>
2013-05-19 18:49:26 -0700

    Monit is now a good neighbor

--- installers/MonitLinuxInstaller/src/files/setup.sh
+++ installers/MonitLinuxInstaller/src/files/setup.sh
@@ -93,9 +93,9 @@ chmod 700 monitrc
 if [ -f /etc/monit/monitrc ]; then
     echo_warning "Monit configuration already exists in /etc/monit/monitrc; backing up"
     backup_file /etc/monit/monitrc
+else
+  cp monitrc /etc/monit/monitrc
 fi
-
-cp monitrc /etc/monit/monitrc
 
 if using_mysql; then
  cat >> /etc/monit/monitrc << EOF
