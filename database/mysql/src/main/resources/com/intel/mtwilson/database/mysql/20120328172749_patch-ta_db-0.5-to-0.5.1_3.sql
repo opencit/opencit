00580bbb616f5a423ebbb4a421c9736e26c6ba02
Ryan Savino <ryanx.k.savino@intel.com>
2013-10-10 11:32:56 -0700

    Commented duplicate CREATE TABLE `tbl_package_namespace` call

--- database/mysql/src/main/resources/com/intel/mtwilson/database/mysql/20120328172749_patch-ta_db-0.5-to-0.5.1_3.sql
+++ database/mysql/src/main/resources/com/intel/mtwilson/database/mysql/20120328172749_patch-ta_db-0.5-to-0.5.1_3.sql
@@ -22,11 +22,11 @@ ALTER TABLE `tbl_mle` ADD UNIQUE KEY `MLE_ID` (`ID`);
 ALTER TABLE `tbl_mle` MODIFY COLUMN `Name` VARCHAR(100) NOT NULL;
 ALTER TABLE `tbl_mle` MODIFY COLUMN `Version` VARCHAR(100) NOT NULL;
 
-CREATE TABLE `tbl_package_namespace` (
+/*CREATE TABLE `tbl_package_namespace` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `Name` varchar(45) NOT NULL,
   `VendorName` varchar(45) NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;*/
 
-INSERT INTO `changelog` (`ID`, `APPLIED_AT`, `DESCRIPTION`) VALUES (20120328172745,NOW(),'premium - create 0.5.1 schema module package namespace');
+INSERT INTO `changelog` (`ID`, `APPLIED_AT`, `DESCRIPTION`) VALUES (20120328172749,NOW(),'premium - create 0.5.1 schema module package namespace');
