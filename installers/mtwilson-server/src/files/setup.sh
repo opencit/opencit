5cff9584f57f1eb01fc46f83c1449bd2ca9163f2
Ryan Savino <ryanx.k.savino@intel.com>
2013-11-24 12:17:59 -0800

    fixed writing database variables to properties files

--- installers/mtwilson-server/src/files/setup.sh
+++ installers/mtwilson-server/src/files/setup.sh
@@ -51,7 +51,7 @@ load_conf
 load_defaults
 
 if [[ $MTWILSON_OWNER == "glassfish" || $MTWILSON_OWNER == "tomcat" ]]; then
- echo_warnring "Program files are writable by the web service container, this is a possible security issue"
+ echo_warning "Program files are writable by the web service container, this is a possible security issue"
 else
  ret=false
  getent passwd $MTWILSON_OWNER >/dev/null && ret=true
@@ -223,16 +223,8 @@ update_property_in_file "mtwilson.as.autoUpdateHost" /etc/intel/cloudsecurity/mt
 #Save variables to properties file
 if using_mysql; then   
   mysql_write_connection_properties /etc/intel/cloudsecurity/mtwilson.properties mtwilson.db
-  mysql_write_connection_properties /etc/intel/cloudsecurity/attestation-service.properties mountwilson.as.db
-  mysql_write_connection_properties /etc/intel/cloudsecurity/audit-handler.properties mountwilson.audit.db
-  mysql_write_connection_properties /etc/intel/cloudsecurity/management-service.properties mountwilson.ms.db
-  mysql_write_connection_properties /etc/intel/cloudsecurity/wlm-service.properties mountwilson.as.db
 elif using_postgres; then
   postgres_write_connection_properties /etc/intel/cloudsecurity/mtwilson.properties mtwilson.db
-  postgres_write_connection_properties /etc/intel/cloudsecurity/attestation-service.properties mountwilson.as.db
-  postgres_write_connection_properties /etc/intel/cloudsecurity/audit-handler.properties mountwilson.audit.db
-  postgres_write_connection_properties /etc/intel/cloudsecurity/management-service.properties mountwilson.ms.db
-  postgres_write_connection_properties /etc/intel/cloudsecurity/wlm-service.properties mountwilson.as.db
 fi
 
 # copy default logging settings to /etc
