7447e9e948ed9f5e4a6fa0f84b86bf30d21c523c
Jonathan Buhacoff <jonathan.buhacoff@intel.com>
2013-12-04 09:16:29 -0800

    first fix for issue #1038 cross site request forgery is to ensure referring site is same as current site

--- portals/mtwilson-portal/src/main/java/com/intel/mountwilson/security/SecurityFilter.java
+++ portals/mtwilson-portal/src/main/java/com/intel/mountwilson/security/SecurityFilter.java
@@ -1,6 +1,7 @@
 package com.intel.mountwilson.security;
 
 import java.io.IOException;
+import java.net.URL;
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -32,14 +33,37 @@ public class SecurityFilter implements Filter {
 				boolean logged =  (Boolean) obj;
 				System.err.println(logged);
 				if (logged) {
-					log.debug("User is logged in, forwarding request to "+url);
-					chain.doFilter(request, response);
+                    // fix for issue #1038 cross site request forgery (CSRF) is to ensure that logged-in users are in control of requests
+                    // so first we deny any request coming from a source other than a link or form in the portal by ensuring the referer 
+                    // header contains our own address;  there are situations where attackers can circumvent this  
+                    // but it's an easy first line of defense before we spend any resources checking something more intensive
+                    log.debug("CSRF: referer = {}", request.getHeader("Referer")); // example CSRF: referer = http://localhost:8080/mtwilson-portal/home.html
+                    log.debug("CSRF: request = {}", request.getRequestURL().toString()); // example CSRF: request = http://localhost:8080/mtwilson-portal/getData/getDashBoardData.html
+                    try {
+                        URL refererUrl = new URL(request.getHeader("Referer"));
+                        URL requestUrl = new URL(request.getRequestURL().toString());
+                        boolean refererOk = requestUrl.getProtocol().equals(refererUrl.getProtocol()) 
+                                && requestUrl.getHost().equals(refererUrl.getHost()) 
+                                && requestUrl.getPort() == refererUrl.getPort() ;
+                        if( refererOk ) {
+                            log.debug("User is logged in, forwarding request to "+url);
+                            chain.doFilter(request, response);
+                        }
+                        else {
+                            log.debug("Invalid request, possible CSRF because referer does not match request URL; forwarding request to Login page.");
+                            goToLogin(request,response);
+                        }
+                    }
+                    catch(Exception e) {
+                        log.debug("Invalid request, forwarding request to Login page.", e);
+                        goToLogin(request,response);
+                    }
 				}else {
-					log.info("User is not logged in or session expired, forwarding request to Login page.");
+					log.debug("User is not logged in or session expired, forwarding request to Login page.");
 					goToLogin(request,response);
 				}
 			}else {
-				log.info("User is not logged in or session expired, forwarding request to Login page.");
+				log.debug("User is not logged in or session expired, forwarding request to Login page.");
 				goToLogin(request,response);
 			}
 			
