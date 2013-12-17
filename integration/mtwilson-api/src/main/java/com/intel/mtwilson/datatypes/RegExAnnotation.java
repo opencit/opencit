fe9abd83885bec7d2df1abbb8b408bb42cbc7339
Sudhir S Bangalore <sudhir.s.bangalore@intel.com>
2013-12-17 15:08:11 +0530

    Updated the default regex to fix a bug where in the whitelist was not getting verified correctly.

--- integration/mtwilson-api/src/main/java/com/intel/mtwilson/datatypes/RegExAnnotation.java
+++ integration/mtwilson-api/src/main/java/com/intel/mtwilson/datatypes/RegExAnnotation.java
@@ -9,7 +9,7 @@ package com.intel.mtwilson.datatypes;
  * @author ssbangal
  */
 public class RegExAnnotation {
-    public static final String DEFAULT_PATTERN = "^([a-zA-Z0-9_\\.\\, -]+)$";
+    public static final String DEFAULT_PATTERN = "^([a-zA-Z0-9_\\.\\, |-]+)$";
     public static final String IPADDRESS_PATTERN = "^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$";
     public static final String FQDN_PATTERN = "^(([a-zA-Z]|[a-zA-Z][a-zA-Z0-9\\-]*[a-zA-Z0-9])\\.)*([A-Za-z]|[A-Za-z][A-Za-z0-9\\-]*[A-Za-z0-9])$";
     public static final String IPADDR_FQDN_PATTERN = "(^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$)|(^(([a-zA-Z]|[a-zA-Z][a-zA-Z0-9\\-]*[a-zA-Z0-9])\\.)*([A-Za-z]|[A-Za-z][A-Za-z0-9\\-]*[A-Za-z0-9])$)";
