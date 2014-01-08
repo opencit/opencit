3394992a2c77487342789e7dced6518f02df4c62
Jonathan Buhacoff <jonathan.buhacoff@intel.com>
2014-01-08 01:32:43 -0800

    added dependency on mtwilson-jersey-util and sample model/resources in attestation service

---
+++ services/AttestationService/src/main/java/com/intel/mtwilson/as/rest/v2/model/File.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2013 Intel Corporation
+ * All rights reserved.
+ */
+package com.intel.mtwilson.as.rest.v2.model;
+
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+import com.intel.mtwilson.jersey.Document;
+
+/**
+ * 
+ * @author jbuhacoff
+ */
+@JacksonXmlRootElement(localName="file")
+public class File extends Document {
+    private String name;
+    private String content;
+    private String contentType;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContentType() {
+        return contentType;
+    }
+
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    
+}
