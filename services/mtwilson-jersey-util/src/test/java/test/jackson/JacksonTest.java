764d15d696045253fbc0c8d4f8030e164d5f5367
Jonathan Buhacoff <jonathan.buhacoff@intel.com>
2014-01-14 12:33:36 -0800

    added xml examples

--- services/mtwilson-jersey-util/src/test/java/test/jackson/JacksonTest.java
+++ services/mtwilson-jersey-util/src/test/java/test/jackson/JacksonTest.java
@@ -7,6 +7,8 @@ package test.jackson;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -17,10 +19,12 @@ import static org.junit.Assert.*;
 public class JacksonTest {
     private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(JacksonTest.class);
     
+    @JacksonXmlRootElement(localName="fruit")
     public static class Fruit {
         public String fruitName;
         public String fruitColor;
     }
+    
     @Test
     public void testWriteDefault() throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
@@ -43,6 +47,31 @@ public class JacksonTest {
         String json = mapper.writeValueAsString(fruit);
         log.debug(json); // {"fruit_name":"apple","fruit_color":"red"}
         assertEquals("{\"fruit_name\":\"apple\",\"fruit_color\":\"red\"}", json);
+    }
+    
+    
+    @Test
+    public void testWriteUnderscoresXml() throws JsonProcessingException {
+        XmlMapper mapper = new XmlMapper();
+        mapper.setPropertyNamingStrategy(new PropertyNamingStrategy.LowerCaseWithUnderscoresStrategy());
+        Fruit fruit = new Fruit();
+        fruit.fruitName = "apple";
+        fruit.fruitColor = "red";
+        String xml = mapper.writeValueAsString(fruit);
+        log.debug(xml); // <fruit><fruit_name>apple</fruit_name><fruit_color>red</fruit_color></fruit>
+        assertEquals("<fruit><fruit_name>apple</fruit_name><fruit_color>red</fruit_color></fruit>", xml);
+    }
+    
+    @Test
+    public void testReadUnderscoresXml() throws Exception {
+        String xml = "<fruit><fruit_name>apple</fruit_name><fruit_color>red</fruit_color></fruit>";
+        XmlMapper mapper = new XmlMapper();
+        mapper.setPropertyNamingStrategy(new PropertyNamingStrategy.LowerCaseWithUnderscoresStrategy());
+        Fruit fruit = mapper.readValue(xml, Fruit.class);
+        log.debug("color: {}", fruit.fruitColor);
+        log.debug("name: {}", fruit.fruitName);
+        assertEquals("red", fruit.fruitColor);
+        assertEquals("apple", fruit.fruitName);
     }
     
     /**
