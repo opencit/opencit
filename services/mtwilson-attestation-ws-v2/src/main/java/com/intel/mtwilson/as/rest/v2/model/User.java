5ce197881e9f45edc7ded83e2c9b4bfef95076ec
Sudhir S Bangalore <sudhir.s.bangalore@intel.com>
2014-01-17 15:57:49 +0530

    dev-2.0-sprint-1-api-v2-sudhir : API V2 for managing mw_portal_user table.

---
+++ services/mtwilson-attestation-ws-v2/src/main/java/com/intel/mtwilson/as/rest/v2/model/User.java
@@ -0,0 +1,73 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.intel.mtwilson.as.rest.v2.model;
+
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+import com.intel.mtwilson.jersey.Document;
+
+/**
+ *
+ * @author ssbangal
+ */
+// The JacksonXmlRootElement(localName="user") annotation is responsible for the lowercase "user" tag, otherwise the default would be "User"
+@JacksonXmlRootElement(localName="user")
+public class User extends Document{
+    private String name;
+    private Boolean enabled;
+    private String status;
+    private String comments;
+    private String locale;
+    private byte[] keystore;
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Boolean getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+
+    public String getLocale() {
+        return locale;
+    }
+
+    public void setLocale(String locale) {
+        this.locale = locale;
+    }
+
+    public byte[] getKeystore() {
+        return keystore;
+    }
+
+    public void setKeystore(byte[] keystore) {
+        this.keystore = keystore;
+    }
+    
+    
+}
