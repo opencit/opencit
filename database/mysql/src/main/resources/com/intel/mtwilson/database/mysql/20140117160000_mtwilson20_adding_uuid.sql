72cdf7197405428bdd5446f66fee7be6ff89ee04
Sudhir S Bangalore <sudhir.s.bangalore@intel.com>
2014-01-23 16:31:35 +0530

    dev-2.0-sprint-1: Changes for the mle_source, mle_pcr_manifest & mle_module_manifest tables.

--- database/mysql/src/main/resources/com/intel/mtwilson/database/mysql/20140117160000_mtwilson20_adding_uuid.sql
+++ database/mysql/src/main/resources/com/intel/mtwilson/database/mysql/20140117160000_mtwilson20_adding_uuid.sql
@@ -19,6 +19,18 @@ UPDATE mw_mle SET uuid_hex = (SELECT uuid());
 
 -- We need to use the UUID from the mw_mle table to update the mw_mle_source table
 ALTER TABLE `mw_as`.`mw_mle_source` ADD COLUMN `uuid_hex` CHAR(36) NOT NULL  AFTER `ID` ;
-UPDATE mw_mle_source ms SET uuid_hex = (SELECT m.uuid_hex FROM mw_mle m WHERE m.ID = ms.MLE_ID);
+UPDATE mw_mle_source SET uuid_hex = (SELECT uuid());
+ALTER TABLE `mw_as`.`mw_mle_source` ADD COLUMN `mle_uuid_hex` CHAR(36) NOT NULL  AFTER `uuid_hex` ;
+UPDATE mw_mle_source ms SET mle_uuid_hex = (SELECT m.uuid_hex FROM mw_mle m WHERE m.ID = ms.MLE_ID);
+
+ALTER TABLE `mw_as`.`mw_pcr_manifest` ADD COLUMN `uuid_hex` CHAR(36) NOT NULL  AFTER `ID` ;
+UPDATE mw_pcr_manifest SET uuid_hex = (SELECT uuid());
+ALTER TABLE `mw_as`.`mw_pcr_manifest` ADD COLUMN `mle_uuid_hex` CHAR(36) NOT NULL  AFTER `uuid_hex` ;
+UPDATE mw_pcr_manifest mpm SET mle_uuid_hex = (SELECT m.uuid_hex FROM mw_mle m WHERE m.ID = mpm.MLE_ID);
+
+ALTER TABLE `mw_as`.`mw_module_manifest` ADD COLUMN `uuid_hex` CHAR(36) NOT NULL  AFTER `ID` ;
+UPDATE mw_module_manifest SET uuid_hex = (SELECT uuid());
+ALTER TABLE `mw_as`.`mw_module_manifest` ADD COLUMN `mle_uuid_hex` CHAR(36) NOT NULL  AFTER `uuid_hex` ;
+UPDATE mw_module_manifest mpm SET mle_uuid_hex = (SELECT m.uuid_hex FROM mw_mle m WHERE m.ID = mpm.MLE_ID);
 
 INSERT INTO `mw_changelog` (`ID`, `APPLIED_AT`, `DESCRIPTION`) VALUES (20140117160000,NOW(),'Added UUID fields for all the tables');
