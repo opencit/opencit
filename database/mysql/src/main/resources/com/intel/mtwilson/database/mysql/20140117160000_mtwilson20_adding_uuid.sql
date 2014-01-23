14e9ddcae3fc7620cd3a6cef2828cb5880f0a388
Sudhir S Bangalore <sudhir.s.bangalore@intel.com>
2014-01-23 11:29:40 +0530

    dev-2.0-sprint-1: Updated the SQL update script to modify the mw_mle_source table for storing the UUID of the MLE that the entry refers to.

--- database/mysql/src/main/resources/com/intel/mtwilson/database/mysql/20140117160000_mtwilson20_adding_uuid.sql
+++ database/mysql/src/main/resources/com/intel/mtwilson/database/mysql/20140117160000_mtwilson20_adding_uuid.sql
@@ -17,4 +17,8 @@ UPDATE mw_os SET uuid_hex = (SELECT uuid());
 ALTER TABLE `mw_as`.`mw_mle` ADD COLUMN `uuid_hex` CHAR(36) NOT NULL  AFTER `ID` ;
 UPDATE mw_mle SET uuid_hex = (SELECT uuid());
 
+-- We need to use the UUID from the mw_mle table to update the mw_mle_source table
+ALTER TABLE `mw_as`.`mw_mle_source` ADD COLUMN `uuid_hex` CHAR(36) NOT NULL  AFTER `ID` ;
+UPDATE mw_mle_source ms SET uuid_hex = (SELECT m.uuid_hex FROM mw_mle m WHERE m.ID = ms.MLE_ID);
+
 INSERT INTO `mw_changelog` (`ID`, `APPLIED_AT`, `DESCRIPTION`) VALUES (20140117160000,NOW(),'Added UUID fields for all the tables');
