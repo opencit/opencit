99e125773f75209b29a24ff2629e1fdec5b3cfcc
Jonathan Buhacoff <jonathan.buhacoff@intel.com>
2014-01-25 14:24:10 -0800

    dev-2.0-sprint-1: added first draft of rpc resource

---
+++ services/mtwilson-rpc/src/main/java/com/intel/mtwilson/rpc/v2/model/RpcFilterCriteria.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2013 Intel Corporation
+ * All rights reserved.
+ */
+package com.intel.mtwilson.rpc.v2.model;
+
+import com.intel.dcsg.cpg.io.UUID;
+import com.intel.mtwilson.jersey.FilterCriteria;
+import javax.ws.rs.QueryParam;
+
+/**
+ *
+ * @author jbuhacoff
+ */
+public class RpcFilterCriteria implements FilterCriteria<Rpc> {
+    
+    @QueryParam("id")
+    public UUID id;
+    @QueryParam("nameEqualTo")
+    public String nameEqualTo;
+    @QueryParam("nameContains")
+    public String nameContains;
+    
+    // TODO also allow administrators to filter by user,
+    // createdOn, updatedOn, whether it has inline input or a link to a file,
+    // current progress, or status
+}
