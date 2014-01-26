0b96a6c30c74fa03c4035b0bbc263f12a8f1d66a
Jonathan Buhacoff <jonathan.buhacoff@intel.com>
2014-01-26 01:42:27 -0800

    dev-2.0-sprint-1: added mtwilson-version using java templates to provide maven build info, and mtwilson-version-ws-v2 to make version info available via api

---
+++ integration/mtwilson-version/src/test/java/com/intel/mtwilson/version/VersionTest.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (C) 2013 Intel Corporation
+ * All rights reserved.
+ */
+package com.intel.mtwilson.version;
+
+import com.intel.mtwilson.Version;
+import org.junit.Test;
+
+/**
+ *
+ * @author jbuhacoff
+ */
+public class VersionTest {
+    private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(VersionTest.class);
+
+    @Test
+    public void testVersion() {
+        log.debug("Version: {}", Version.getInstance());
+    }
+}
