da1b0123645d1fee5b7b8565d7d36e04ec1590ee
Ryan Savino <ryanx.k.savino@intel.com>
2014-01-26 18:01:16 -0800

    dev-2.0-sprint-1-i18n: started writing the post api call for locales

--- services/ManagementService/src/main/java/com/intel/mtwilson/ms/rest/i18n.java
+++ services/ManagementService/src/main/java/com/intel/mtwilson/ms/rest/i18n.java
@@ -5,6 +5,8 @@
 package com.intel.mtwilson.ms.rest;
 
 import com.intel.mtwilson.My;
+import com.intel.mtwilson.ms.common.common.MCPersistenceManager;
+import com.intel.mtwilson.ms.controller.MwPortalUserJpaController;
 import com.intel.mtwilson.security.annotations.RolesAllowed;
 import java.io.IOException;
 import javax.ejb.Stateless;
@@ -40,14 +42,21 @@ public class i18n {
     public String[] getLocales() throws IOException {
         return My.configuration().getAvailableLocales();
     }
-//    @POST
-//    @Produces(MediaType.TEXT_PLAIN)
-//    @Consumes("application/json")
-//    @Path("/locales")
-//    @RolesAllowed({"Security"})
-//    public String setLocaleForUser(ApiClientCreateRequest apiClientRequest) {
-//        ValidationUtil.validate(apiClientRequest);
+    
+    
+    @POST
+    @Produces(MediaType.TEXT_PLAIN)
+    @Consumes("application/json")
+    @Path("/locales")
+    @RolesAllowed({"Security"})
+    public String setLocaleForUser(
+            @QueryParam("api") Boolean api,
+            @QueryParam("username") String username,
+            @QueryParam("locale") String locale) {
+        //ValidationUtil.validate(apiClientRequest);
+//        private MCPersistenceManager mcManager = new MCPersistenceManager();
+//	private MwPortalUserJpaController keystoreJpa = new MwPortalUserJpaController(mcManager.getEntityManagerFactory("MSDataPU"));
 //        new ApiClientBO().create(apiClientRequest);
-//        return "OK";
-//    }
+        return "OK";
+    }
 }
