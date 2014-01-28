e5402d4cef06df137cad504c5a74f0e23d3df2e8
Jonathan Buhacoff <jonathan.buhacoff@intel.com>
2014-01-28 01:48:52 -0800

    dev-2.0-sprint-1: support for x509 certificate content types

--- services/mtwilson-jersey-util/src/main/java/com/intel/mtwilson/jersey/http/OtherMediaType.java
+++ services/mtwilson-jersey-util/src/main/java/com/intel/mtwilson/jersey/http/OtherMediaType.java
@@ -20,10 +20,15 @@ public class OtherMediaType {
     public static final String ENCRYPTED_JAVA = "encrypted/java";
     public static final String APPLICATION_SIGNATURE_OPENSSL = "application/signature.openssl";
     public static final String APPLICATION_SIGNATURE_JAVA = "application/signature.java";
-    public static final String APPLICATION_X_PEM_FILE = "application/x-pem-file";
     public static final String MESSAGE_RFC822 = "message/rfc822";
     public static final String MULTIPART_ENCRYPTED = "multipart/encrypted";
     public static final String MULTIPART_SIGNED = "multipart/signed";
+    // certificate and key exchange formats 
+    public static final String APPLICATION_X_PEM_FILE = "application/x-pem-file";   // .pem 
+    public static final String APPLICATION_PKCS12 = "application/x-pkcs12"; // .p12   .pfx
+    public static final String APPLICATION_PKIX_CERT = "application/pkix-cert"; // http://tools.ietf.org/search/rfc2585  mime type for X509 certificate .cer  DER-encoded
+    public static final String APPLICATION_PKIX_CRL = "application/pkix-crl"; // http://tools.ietf.org/search/rfc2585 mime type for X509 cert revocation list .crl   DER-encoded
+    public static final String APPLICATION_PKCS7_MIME = "application/pkcs7-mime"; // http://tools.ietf.org/search/rfc2797  CMS enrollment messages  .p7c, .p7m, 
     // compression formats
     public static final String APPLICATION_ZIP = "application/zip";
     public static final String APPLICATION_GZIP = "application/gzip"; // replaces application/x-gzip, application/x-gzip-compressed, etc. http://www.ietf.org/rfc/rfc6713.txt
