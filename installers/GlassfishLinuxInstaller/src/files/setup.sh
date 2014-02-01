ca7bf2a461ef7b7c43bf947645b5ab1632ac142e
Ryan Savino <ryanx.k.savino@intel.com>
2014-01-31 18:42:46 -0800

    dev-2.0-sprint-1: moved glassfish commands to glassfish setup.sh because asadmin cannot be called when glassfish service is not running

--- installers/GlassfishLinuxInstaller/src/files/setup.sh
+++ installers/GlassfishLinuxInstaller/src/files/setup.sh
@@ -20,6 +20,12 @@ cp jackson-core-asl.jar ${GLASSFISH_HOME}/modules/
 cp jackson-mapper-asl.jar ${GLASSFISH_HOME}/modules/
 cp jackson-xc.jar ${GLASSFISH_HOME}/modules/
 
+asenvFile=`find "$GLASSFISH_HOME" -name asenv.conf`
+echo "AS_JAVA=$JAVA_HOME" >> "$asenvFile"
+echo "Increasing glassfish max thread pool size to 200..."
+$glassfish_bin set server.thread-pools.thread-pool.http-thread-pool.max-thread-pool-size=200
+echo "Enabling secure admin for glassfish..."
+$glassfish_bin enable-secure-admin
 
 # on installations configured to use mysql, the customer is responsible for 
 # providing the java mysql connector before starting the mt wilson installer.
