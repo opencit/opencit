5f2e6ab26906ffb79228ce4223bfb486d27b57b1
Sudhir S Bangalore <sudhir.s.bangalore@intel.com>
2014-02-05 16:15:51 +0530

    dev-2.0-sprint-1: Update MleModule APIs to extend AbstractJsonapiResource.

---
+++ services/mtwilson-attestation-ws-v2/src/main/java/com/intel/mtwilson/as/rest/v2/model/MleModuleLocator.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.intel.mtwilson.as.rest.v2.model;
+
+import com.intel.dcsg.cpg.io.UUID;
+import com.intel.mtwilson.jersey.Locator;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
+
+/**
+ *
+ * @author ssbangal
+ */
+public class MleModuleLocator implements Locator<MleModule> {
+
+    @PathParam("mle-id")
+    public UUID mleUuid;
+    @QueryParam("id")
+    public UUID moduleUuid;
+    
+    @Override
+    public void copyTo(MleModule item) {
+        item.setMleUuid(mleUuid.toString());
+        item.setId(moduleUuid);
+    }
+    
+}
