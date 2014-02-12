a4e5c1747bcb968a69f619d75bda13825a17b27e
Ryan Savino <ryanx.k.savino@intel.com>
2014-02-12 13:00:17 -0800

    dev-2.0-sprint-1-i18n-noerr: removed org.codehaus.jackson and replaced with com.fasterxml.jackson annotation reference to JsonSerialize

--- services/mtwilson-attestation-ws-v2/src/main/java/com/intel/mtwilson/as/rest/v2/model/HostAikCertificateCollection.java
+++ services/mtwilson-attestation-ws-v2/src/main/java/com/intel/mtwilson/as/rest/v2/model/HostAikCertificateCollection.java
@@ -5,12 +5,13 @@
 package com.intel.mtwilson.as.rest.v2.model;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 import com.intel.mtwilson.jersey.DocumentCollection;
 import java.util.ArrayList;
 import java.util.List;
-import org.codehaus.jackson.map.annotate.JsonSerialize;
+//import org.codehaus.jackson.map.annotate.JsonSerialize;
 
 /**
  *
