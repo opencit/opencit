87097e389de6326819b44e3626b8868853ec621f
Ryan Savino <ryanx.k.savino@intel.com>
2014-02-23 23:29:57 -0800

    dev-2.0-sprint-1-atag-merge: added NamedQuery findByHwUUID, left out during merge

--- services/mtwilson-attestation-jpa/src/main/java/com/intel/mtwilson/as/data/TblHosts.java
+++ services/mtwilson-attestation-jpa/src/main/java/com/intel/mtwilson/as/data/TblHosts.java
@@ -44,6 +44,7 @@ import org.slf4j.LoggerFactory;
 //    @NamedQuery(name = "TblHosts.findByUpdatedOn", query = "SELECT t FROM TblHosts t WHERE t.updatedOn = :updatedOn"),
     @NamedQuery(name = "TblHosts.findByErrorCode", query = "SELECT t FROM TblHosts t WHERE t.errorCode = :errorCode"),
     @NamedQuery(name = "TblHosts.findByErrorDescription", query = "SELECT t FROM TblHosts t WHERE t.errorDescription = :errorDescription"),
+    @NamedQuery(name = "TblHosts.findByHwUUID", query = "SELECT t FROM TblHosts t WHERE t.hardware_uuid = :hardware_uuid"),
     @NamedQuery(name = "TblHosts.findByUuidHex", query = "SELECT t FROM TblHosts t WHERE t.uuid_hex = :uuid_hex"),
     @NamedQuery(name = "TblHosts.findByDescriptionSearchCriteria", query = "SELECT t FROM TblHosts t WHERE t.description like :search"),
     @NamedQuery(name = "TblHosts.findByNameSearchCriteria", query = "SELECT t FROM TblHosts t WHERE t.name like :search")})
