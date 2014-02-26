1de450c99de56c1878b94a011be905117e99d170
Ryan Savino <ryanx.k.savino@intel.com>
2014-02-26 01:28:19 -0800

    dev-2.0-sprint-1-atag-merge: moved java symlink for jar libs to after webserver_detect

--- installers/mtwilson-server/src/files/setup.sh
+++ installers/mtwilson-server/src/files/setup.sh
@@ -244,24 +244,11 @@ cp logback-stderr.xml /etc/intel/cloudsecurity
 chmod 700 shiro.ini
 cp shiro.ini /etc/intel/cloudsecurity
 
-echo "Adding symlinks..."
+echo "Adding symlink for /opt/mtwilson/configuration..."
 # temp symlink -- SAVY added 2014-02-26
 if [[ ! -h "/opt/mtwilson/configuration" ]]; then
   mkdir -p /opt/mtwilson
   ln -s "/etc/intel/cloudsecurity" "/opt/mtwilson/configuration"
-fi
-if using_glassfish; then
-  # temp symlink -- SAVY added 2014-02-04
-  if [[ ! -h "/opt/mtwilson/java" ]]; then
-    mkdir -p /opt/mtwilson
-    ln -s "$GLASSFISH_HOME/domains/domain1/applications/mtwilson/WEB-INF/lib" "/opt/mtwilson/java"
-  fi
-elif using_tomcat; then
-  # temp symlink -- SAVY added 2014-02-04
-  if [[ ! -h "/opt/mtwilson/java" ]]; then
-    mkdir -p /opt/mtwilson
-    ln -s "$TOMCAT_HOME/webapps/mtwilson/WEB-INF/lib" "/opt/mtwilson/java"
-  fi
 fi
 
 
@@ -620,6 +607,21 @@ elif using_tomcat; then
     echo_warning "Skipping webservice init"
   fi
  
+fi
+
+echo "Adding symlink for /opt/mtwilson/java..."
+if using_glassfish; then
+  # temp symlink -- SAVY added 2014-02-04
+  if [[ ! -h "/opt/mtwilson/java" ]]; then
+    mkdir -p /opt/mtwilson
+    ln -s "$GLASSFISH_HOME/domains/domain1/applications/mtwilson/WEB-INF/lib" "/opt/mtwilson/java"
+  fi
+elif using_tomcat; then
+  # temp symlink -- SAVY added 2014-02-04
+  if [[ ! -h "/opt/mtwilson/java" ]]; then
+    mkdir -p /opt/mtwilson
+    ln -s "$TOMCAT_HOME/webapps/mtwilson/WEB-INF/lib" "/opt/mtwilson/java"
+  fi
 fi
 
 if [ ! -z "$opt_privacyca" ]; then
