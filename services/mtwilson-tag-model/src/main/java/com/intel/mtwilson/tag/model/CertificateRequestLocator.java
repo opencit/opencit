914c146313d11cc159e238adb1569a4a8fc779f8
Jonathan Buhacoff <jonathan.buhacoff@intel.com>
2014-03-14 11:23:17 -0700

    dev-2.0-sprint-2: certificate request now accepts xml and encrypted xml with subject in http header or query string

--- services/mtwilson-tag-model/src/main/java/com/intel/mtwilson/tag/model/CertificateRequestLocator.java
+++ services/mtwilson-tag-model/src/main/java/com/intel/mtwilson/tag/model/CertificateRequestLocator.java
@@ -7,6 +7,7 @@ package com.intel.mtwilson.tag.model;
 import com.intel.dcsg.cpg.io.UUID;
 import com.intel.mtwilson.jersey.Locator;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
 
 /**
  *
@@ -16,10 +17,16 @@ public class CertificateRequestLocator implements Locator<CertificateRequest>{
 
     @PathParam("id")
     public UUID id;
+    
+    @QueryParam("subject")
+    public String subject;
 
     @Override
     public void copyTo(CertificateRequest item) {
         item.setId(id);
+        if( subject != null && !subject.isEmpty() && item.getSubject() == null ) {
+            item.setSubject(subject);
+        }
     }
     
 }
--- services/mtwilson-tag-ws-v2/src/main/java/com/intel/mtwilson/tag/rest/v2/resource/CertificatesRequests.java
+++ services/mtwilson-tag-ws-v2/src/main/java/com/intel/mtwilson/tag/rest/v2/resource/CertificatesRequests.java
@@ -4,15 +4,27 @@
  */
 package com.intel.mtwilson.tag.rest.v2.resource;
 
+import com.intel.dcsg.cpg.io.UUID;
 import com.intel.mtwilson.tag.model.CertificateRequest;
 import com.intel.mtwilson.tag.model.CertificateRequestCollection;
 import com.intel.mtwilson.tag.model.CertificateRequestFilterCriteria;
 import com.intel.mtwilson.tag.model.CertificateRequestLocator;
 import com.intel.mtwilson.jersey.NoLinks;
+import com.intel.mtwilson.jersey.http.OtherMediaType;
 import com.intel.mtwilson.jersey.resource.AbstractJsonapiResource;
 import com.intel.mtwilson.launcher.ws.ext.V2;
 import com.intel.mtwilson.tag.rest.v2.repository.CertificateRequestRepository;
+import java.nio.charset.Charset;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.BeanParam;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response.Status;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -40,5 +52,54 @@ public class CertificatesRequests extends AbstractJsonapiResource<CertificateReq
     protected CertificateRequestRepository getRepository() {
         return repository;
     }
-        
+    
+    @Override
+    @Consumes({MediaType.APPLICATION_JSON, OtherMediaType.APPLICATION_YAML, OtherMediaType.TEXT_YAML})
+    @Produces({MediaType.APPLICATION_JSON, OtherMediaType.APPLICATION_YAML, OtherMediaType.TEXT_YAML})
+    @POST
+    public CertificateRequest createOne(@BeanParam CertificateRequestLocator locator, CertificateRequest item) {
+        return super.createOne(locator, item);
+    }
+
+    /**
+     * Because the selection xml format (plaintext or encrypted) does not
+     * include the target host's subject uuid, the client must specify
+     * the target host subject uuid either with an HTTP header "Subject" whose
+     * value is the uuid, or with a query parameter "subject" whose value is
+     * the uuid.  If both are present only the HTTP header is used. If neither
+     * is present the server will return a bad request error.
+     * 
+     * Unlike the JSON API, this method does not return the original request
+     * as the response because the clients that send XML or encrypted XML
+     * don't need it echoed back to them.
+     * 
+     * @param locator
+     * @param message
+     * @param request 
+     */
+    @Consumes({MediaType.APPLICATION_XML, OtherMediaType.MESSAGE_RFC822})
+    @POST
+    public void createOneXml(@BeanParam CertificateRequestLocator locator, byte[] message, @Context HttpServletRequest request) {
+        CertificateRequest certificateRequest = new CertificateRequest();
+        certificateRequest.setId(new UUID());
+        certificateRequest.setStatus("New");
+        certificateRequest.setSubject(getSubject(request, locator));
+        certificateRequest.setContent(message);
+        certificateRequest.setContentType(request.getContentType()); // either application/xml or message/rfc822
+        getRepository().create(certificateRequest);
+    }
+    
+    private String getSubject(HttpServletRequest request, CertificateRequestLocator locator) {
+        String subject = request.getHeader("Subject");
+        if( subject != null && !subject.isEmpty()) {
+            return subject; 
+        }
+        else if( locator.subject != null && !locator.subject.isEmpty() ) {
+            return locator.subject; // from query paramter  ?subject={subject}            
+        }
+        else {
+            throw new WebApplicationException(Status.BAD_REQUEST); // TODO:  ErrorCode enum with internationalized message saying subject is required in header or query param
+        }
+    }
+    
 }
