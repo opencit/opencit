09bc7d8cc20043ecb681034f120ad4a3c085dfe2
Sudhir S Bangalore <sudhir.s.bangalore@intel.com>
2014-03-20 01:31:24 +0530

    dev-2.0-sprint-2: Removed the dependency on the selections/certificate table from the CertificateRequest table. CertificateRequest would be used only for requests that would need external CA approval.

--- services/mtwilson-tag-model/src/main/java/com/intel/mtwilson/tag/model/CertificateRequest.java
+++ services/mtwilson-tag-model/src/main/java/com/intel/mtwilson/tag/model/CertificateRequest.java
@@ -17,10 +17,6 @@ public class CertificateRequest extends Document{
         
     private String subject; // from the query parameter   POST /certificate-requests?subject={my-hardware-uuid}   and request body is the selection xml or json 
     private String status;
-    private String authorityName; // TODO: remove,  instead of associating old requests to current certs, just mark requests complete.  certs can be searched on the subject to correlate.
-    private UUID certificateId; // TODO: remove,  instead of associating old requests to current certs, just mark requests complete. certs can be searched on the subject to correlate.
-    private UUID selectionId; // TODO: remove, and instead of having a field here we should generate the selection xml and store it.  <selections><selection id="..."/></selections>  so the approver can have a consistent input.
-    private String selectionName; //remove, and instead of having a field here we should generate the selection xml and store it.  <selections><selection name="..."/></selections>  so the approver can have a consistent input.
     private byte[] content;// the selections xml format;  may be encrypted
     private String contentType; //   application/xml for plain xml,  message/rfc822 for the encrypted xml with headers,  application/json  for the json request from the UI
     
@@ -38,39 +34,6 @@ public class CertificateRequest extends Document{
 
     public void setStatus(String status) {
         this.status = status;
-    }
-
-    public String getAuthorityName() {
-        return authorityName;
-    }
-
-    public void setAuthorityName(String authorityName) {
-        this.authorityName = authorityName;
-    }
-
-    public UUID getCertificateId() {
-        return certificateId;
-    }
-
-    public void setCertificateId(UUID certificateId) {
-        this.certificateId = certificateId;
-    }
-
-    public UUID getSelectionId() {
-        return selectionId;
-    }
-
-    public void setSelectionId(UUID selectionId) {
-        this.selectionId = selectionId;
-    }
-
-    
-    public String getSelectionName() {
-        return selectionName;
-    }
-
-    public void setSelectionName(String selectionName) {
-        this.selectionName = selectionName;
     }
 
     public byte[] getContent() {
