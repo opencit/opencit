fb76fa5c1bbc61efe7a48769d73dab1d32415952
Ryan Savino <ryan.k.savino@intel.com>
2014-03-20 09:54:27 -0700

    dev-2.0-sprint-2: added path to cert for retrieving fingerprint moved ApproveMtWilsonClient to mtwilson-tag-setup

--- installers/mtwilson-server/src/files/setup.sh
+++ installers/mtwilson-server/src/files/setup.sh
@@ -757,8 +757,8 @@ call_tag_setupcommand TagCreateCaKey "CN=assetTagService"
 call_tag_setupcommand TagExportFile cacerts | grep -v ":" > $CONFIG_DIR/AssetTagCA.pem
 call_tag_setupcommand TagCreateMtWilsonClient --url="$MTWILSON_API_BASEURL" --username="$MTWILSON_TAG_API_USER" --password="$MTWILSON_TAG_API_PASS"
 
-fingerprint=`openssl dgst -sha1 serverAtag.cer | awk -F= '{print $2}' | sed -e 's/^ *//' -e 's/ *$//'`
-call_setupcommand ApproveMtwilsonClient --fingerprint="$fingerprint"
+fingerprint=`openssl dgst -sha1 $CONFIG_DIR/serverAtag.cer | awk -F= '{print $2}' | sed -e 's/^ *//' -e 's/ *$//'`
+call_tag_setupcommand ApproveMtwilsonClient --fingerprint="$fingerprint"
 
 ##############################################################################################################################################################################
 
