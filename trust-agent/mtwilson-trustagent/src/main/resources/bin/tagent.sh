652e50332dc6dc11d2aa330865b6e28d74462226
Ryan Savino <ryan.k.savino@intel.com>
2014-04-10 21:00:19 -0700

    dev-2.0-sprint-2-pcafix: bug fixes

--- trust-agent/mtwilson-trustagent/src/main/resources/bin/tagent.sh
+++ trust-agent/mtwilson-trustagent/src/main/resources/bin/tagent.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
-#
-# chkconfig: 2345 80 30
-# description: Intel TrustAgent Service
-#
+### BEGIN INIT INFO
+# Provides:          tagent
+# Required-Start:    $remote_fs $syslog
+# Required-Stop:     $remote_fs $syslog
+# Should-Start:      $portmap
+# Should-Stop:       $portmap
+# X-Start-Before:    nis
+# X-Stop-After:      nis
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# X-Interactive:     true
+# Short-Description: trust agent script
+# Description:       Main script to run trust agent tasks
+### END INIT INFO
+DESC="Trust Agent"
+NAME=tagent
+DAEMON=/opt/trustagent/bin/$NAME
 
 ###################################################################################################
 #Set environment specific variables here 
