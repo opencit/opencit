a06b8ed528af0197743e24105ecc90e825f3a8f4
Ryan Savino <ryan.k.savino@intel.com>
2014-05-01 14:43:01 -0700

    dev-2.0-sprint-3: Merge branch 'dev-2.0-sprint-2' into dev-2.0-sprint-3
    Conflicts:
    	services/mtwilson-tag-setup/src/main/resources/tpa.sh

--- services/mtwilson-tag-setup/src/main/resources/tpa.sh
+++ services/mtwilson-tag-setup/src/main/resources/tpa.sh
@@ -225,7 +225,7 @@ function provisionCert() {
    fi
  fi
 
- if [ -s "$certFile" ]; then
+ if [ ! -s "$certFile" ]; then
    echo "Error downloading asset tag certificate. Check certificate file output here: $certFile"
    echo "Error downloading asset tag certificate. Check certificate file output here: $certFile" > "$tpaDir/completion"
    exit -1
