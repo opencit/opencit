31c18472143344fac9b88933e1bd51c05eb63eb7
Ryan Savino <ryan.k.savino@intel.com>
2014-05-01 14:39:38 -0700

    dev-2.0-sprint-3: sql scripts to make aik_publickey in mw_hosts a unique key

---
+++ database/postgresql/src/main/resources/com/intel/mtwilson/database/postgresql/20140430140500_mtwilson20_unique_aikpublickey.sql
@@ -0,0 +1,7 @@
+-- created 2014-04-30
+-- rksavino
+-- Makes aik_publickey a unique key in mw_hosts
+
+ALTER TABLE mw_hosts ADD CONSTRAINT mw_host_aik_public_key_unique UNIQUE (aik_publickey);
+  
+INSERT INTO mw_changelog (ID, APPLIED_AT, DESCRIPTION) VALUES (20140430140500,NOW(),'Patch to make aik_publickey a unique key in mw_hosts.');
