c72bb66d8f89eb6f2f7b5dad4c1d0024b63d6572
Sudhir S Bangalore <sudhir.s.bangalore@intel.com>
2014-05-01 18:48:20 +0530

    dev-2.0-sprint-3: Added wrapper classes to send the data from api client library to the v2 RPC APIs

--- services/mtwilson-attestation-model/src/main/java/com/intel/mtwilson/as/rest/v2/model/CreateWhiteListRpcInput.java
+++ services/mtwilson-attestation-model/src/main/java/com/intel/mtwilson/as/rest/v2/model/CreateWhiteListRpcInput.java
@@ -6,16 +6,14 @@ package com.intel.mtwilson.as.rest.v2.model;
 
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 import com.intel.mtwilson.datatypes.TxtHostRecord;
-import com.intel.mtwilson.jersey.Document;
 
 /**
  *
  * @author ssbangal
  */
 @JacksonXmlRootElement(localName="create_whitelist_rpc_input")
-public class CreateWhiteListRpcInput /*extends Document*/ {
+public class CreateWhiteListRpcInput {
     private TxtHostRecord host;
-//    private boolean result;  // result should not in the input class ; 
 
     public TxtHostRecord getHost() {
         return host;
@@ -23,15 +21,5 @@ public class CreateWhiteListRpcInput /*extends Document*/ {
 
     public void setHost(TxtHostRecord host) {
         this.host = host;
-    }
-/*
-    public boolean isResult() {
-        return result;
-    }
-
-    public void setResult(boolean result) {
-        this.result = result;
-    }
-*/
-    
+    }    
 }
