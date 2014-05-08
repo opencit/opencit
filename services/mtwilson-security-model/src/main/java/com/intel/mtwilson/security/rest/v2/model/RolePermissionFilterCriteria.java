9e0f0c953c63f3b1efb4f5a1738ca6b971a7c2aa
Sudhir S Bangalore <sudhir.s.bangalore@intel.com>
2014-05-08 09:45:35 +0530

    dev-2.0-sprint-3: Modified the filter criteria of role, role permission and user to return back all the results if the filter criteria is set to false.

--- services/mtwilson-security-model/src/main/java/com/intel/mtwilson/security/rest/v2/model/RolePermissionFilterCriteria.java
+++ services/mtwilson-security-model/src/main/java/com/intel/mtwilson/security/rest/v2/model/RolePermissionFilterCriteria.java
@@ -5,15 +5,19 @@
 package com.intel.mtwilson.security.rest.v2.model;
 
 import com.intel.dcsg.cpg.io.UUID;
+import com.intel.mtwilson.jersey.DefaultFilterCriteria;
 import com.intel.mtwilson.jersey.FilterCriteria;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.QueryParam;
 
 /**
  *
  * @author ssbangal
  */
-public class RolePermissionFilterCriteria implements FilterCriteria<RolePermission> {
+public class RolePermissionFilterCriteria extends DefaultFilterCriteria implements FilterCriteria<RolePermission> {
 
+    @PathParam("role_id")
+    public UUID roleId;
     @QueryParam("id")
     public UUID id;
     @QueryParam("domainEqualTo")
--- services/mtwilson-security-model/src/main/java/com/intel/mtwilson/security/rest/v2/model/UserFilterCriteria.java
+++ services/mtwilson-security-model/src/main/java/com/intel/mtwilson/security/rest/v2/model/UserFilterCriteria.java
@@ -5,6 +5,7 @@
 package com.intel.mtwilson.security.rest.v2.model;
 
 import com.intel.dcsg.cpg.io.UUID;
+import com.intel.mtwilson.jersey.DefaultFilterCriteria;
 import com.intel.mtwilson.jersey.FilterCriteria;
 import javax.ws.rs.QueryParam;
 
@@ -12,7 +13,7 @@ import javax.ws.rs.QueryParam;
  *
  * @author ssbangal
  */
-public class UserFilterCriteria implements FilterCriteria<User> {
+public class UserFilterCriteria extends DefaultFilterCriteria implements FilterCriteria<User> {
 
     @QueryParam("id")
     public UUID id;
