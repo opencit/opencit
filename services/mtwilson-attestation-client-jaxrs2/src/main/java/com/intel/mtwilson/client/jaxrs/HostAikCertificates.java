dd15922e8bac520334c5d03d88079b8af1cc949e
Sudhir S Bangalore <sudhir.s.bangalore@intel.com>
2014-05-09 15:21:12 +0530

    dev-2.0-sprint-3: Created a new project for the Api client library (v2 attestation APIs).

---
+++ services/mtwilson-attestation-client-jaxrs2/src/main/java/com/intel/mtwilson/client/jaxrs/HostAikCertificates.java
@@ -0,0 +1,66 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.intel.mtwilson.client.jaxrs;
+
+import com.intel.mtwilson.client.jaxrs.common.MtWilsonClient;
+import com.intel.mtwilson.as.rest.v2.model.HostAikCertificate;
+import com.intel.mtwilson.as.rest.v2.model.HostAikCertificateCollection;
+import com.intel.mtwilson.as.rest.v2.model.HostAikCertificateFilterCriteria;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Properties;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.MediaType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author ssbangal
+ */
+public class HostAikCertificates extends MtWilsonClient {
+    
+    Logger log = LoggerFactory.getLogger(getClass().getName());
+
+    public HostAikCertificates(URL url) throws Exception{
+        super(url);
+    }
+
+    public HostAikCertificates(Properties properties) throws Exception {
+        super(properties);
+    }
+    
+    public HostAikCertificateCollection searchHostAikCertificates(HostAikCertificateFilterCriteria criteria) {
+        log.debug("target: {}", getTarget().getUri().toString());
+        HashMap<String,Object> map = new HashMap<String,Object>();
+        map.put("host_id", criteria.hostUuid);
+        HostAikCertificateCollection objCollection = getTargetPathWithQueryParams("hosts/{host_id}/aik-certificates", criteria)
+                .resolveTemplates(map).request(MediaType.APPLICATION_JSON).get(HostAikCertificateCollection.class);
+        return objCollection;
+    }
+    
+    public HostAikCertificate retrieveHostAikCertificate(String hostUuid) {
+        log.debug("target: {}", getTarget().getUri().toString());
+        HashMap<String,Object> map = new HashMap<String,Object>();
+        map.put("host_id", hostUuid);
+        // We are passing the host UUID to "id" also even though it will not be used (without this framework treats this call as a 
+        // search call instead of a retrieve call. Currently we support only one aik certificate for a host, we can retrieve
+        // the aik certificate for the host uniquely with the host uuid itself.
+        map.put("id", hostUuid); 
+        HostAikCertificate obj = getTarget().path("hosts/{host_id}/aik-certificates/{id}")
+                .resolveTemplates(map).request(MediaType.APPLICATION_JSON).get(HostAikCertificate.class);
+        return obj;
+    }
+
+    public HostAikCertificate createHostAikCertificate(HostAikCertificate obj) {
+        log.debug("target: {}", getTarget().getUri().toString());
+        HashMap<String,Object> map = new HashMap<String,Object>();
+        map.put("host_id", obj.getHostUuid().toString());
+        HostAikCertificate newObj = getTarget().path("hosts/{host_id}/aik-certificates").resolveTemplates(map)
+                .request().accept(MediaType.APPLICATION_JSON).post(Entity.json(obj), HostAikCertificate.class);
+        return newObj;
+    }
+    
+}
---
+++ services/mtwilson-attestation-client-jaxrs2/src/main/java/com/intel/mtwilson/client/jaxrs/HostTpmPassword.java
@@ -0,0 +1,84 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.intel.mtwilson.client.jaxrs;
+
+import com.intel.mtwilson.client.jaxrs.common.MtWilsonClient;
+import com.intel.dcsg.cpg.configuration.Configuration;
+import com.intel.dcsg.cpg.io.UUID;
+import com.intel.dcsg.cpg.tls.policy.TlsConnection;
+import com.intel.mtwilson.tag.model.TpmPassword;
+import com.intel.mtwilson.tag.model.TpmPasswordCollection;
+import com.intel.mtwilson.tag.model.TpmPasswordFilterCriteria;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.MediaType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author ssbangal
+ */
+public class HostTpmPassword extends MtWilsonClient {
+    
+    Logger log = LoggerFactory.getLogger(getClass().getName());
+
+    public HostTpmPassword(URL url)throws Exception {
+        super(url);
+    }
+
+    public HostTpmPassword(Properties properties) throws Exception {
+        super(properties);
+    }
+    public HostTpmPassword(Configuration configuration) throws Exception {
+        super(configuration);
+    }
+    public HostTpmPassword(Properties configuration, TlsConnection tlsConnection) throws Exception {
+        super(configuration, tlsConnection);
+    }
+    
+    /**
+     * 
+     * @param hostHardwareId
+     * @param tpmOwnerSecretHex
+     * @return the etag for the updated password
+     */
+    public String storeTpmPassword(UUID hostHardwareId, String tpmOwnerSecretHex) {
+        log.debug("target: {}", getTarget().getUri().toString());
+        TpmPassword tpmPassword = new TpmPassword();
+        tpmPassword.setId(hostHardwareId);
+        tpmPassword.setPassword(tpmOwnerSecretHex);
+        TpmPassword result = getTarget()
+                .path("/host-tpm-passwords")
+                .request()
+                .accept(MediaType.APPLICATION_JSON)
+                .post(Entity.json(tpmPassword), TpmPassword.class);
+        if( result.getEtag() != null ) {
+            return result.getEtag();
+        }
+        return null;
+    }
+
+
+    public TpmPassword searchTpmPasswords(TpmPasswordFilterCriteria criteria) {
+        TpmPasswordCollection collection = getTargetPathWithQueryParams("/host-tpm-passwords", criteria).request(MediaType.APPLICATION_JSON).get(TpmPasswordCollection.class);
+        if( collection.getTpmPasswords().isEmpty() ) {
+            return null;
+        }
+        return collection.getTpmPasswords().get(0);
+    }
+
+    
+    public TpmPassword retrieveTpmPassword(UUID hardwareUuid) {
+        HashMap<String,Object> map = new HashMap<>();
+        map.put("id", hardwareUuid.toString());
+        TpmPassword tpmPassword = getTarget().path("/host-tpm-passwords/{id}").resolveTemplates(map).request(MediaType.APPLICATION_JSON).get(TpmPassword.class);
+        return tpmPassword; // may be null
+    }
+
+}
