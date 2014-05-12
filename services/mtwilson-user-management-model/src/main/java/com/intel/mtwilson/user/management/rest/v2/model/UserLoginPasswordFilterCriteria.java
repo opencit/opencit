17a4b20db66133d61a39f24e13e7697318234f14
Sudhir S Bangalore <sudhir.s.bangalore@intel.com>
2014-05-12 08:21:52 +0530

    dev-2.0-sprint-3: Updated the filter criteria and the search method implementation for UserLoginPassword and UserLoginCertificate REST APIs.

--- services/mtwilson-user-management-model/src/main/java/com/intel/mtwilson/user/management/rest/v2/model/UserLoginPasswordFilterCriteria.java
+++ services/mtwilson-user-management-model/src/main/java/com/intel/mtwilson/user/management/rest/v2/model/UserLoginPasswordFilterCriteria.java
@@ -6,6 +6,7 @@ package com.intel.mtwilson.user.management.rest.v2.model;
 
 import com.intel.dcsg.cpg.io.UUID;
 import com.intel.mtwilson.jersey.FilterCriteria;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.QueryParam;
 
 /**
@@ -14,11 +15,13 @@ import javax.ws.rs.QueryParam;
  */
 public class UserLoginPasswordFilterCriteria implements FilterCriteria<UserLoginPassword> {
 
+    @PathParam("user_id")
+    public UUID userUuid;
     @QueryParam("id")
     public UUID id;
-    @QueryParam("userIdEqualTo")
-    public UUID userIdEqualTo;
-    @QueryParam("userNameEqualTo")
-    public String userNameEqualTo;
+    @QueryParam("status")
+    public Status status;
+    @QueryParam("enabled")
+    public Boolean enabled;
     
 }
