bc96c9af6461efede593e93ef8d9ddba371de4cc
Ryan Savino <ryan.k.savino@intel.com>
2014-05-13 09:45:34 -0700

    dev-2.0-sprint-3: removed unique key constraint on aik_publickey column in table mw_hosts so that multiple vmware null values will not conflict

--- database/mysql/src/main/resources/com/intel/mtwilson/database/mysql/20140430140500_mtwilson20_unique_aikpublickey.sql
+++ database/mysql/src/main/resources/com/intel/mtwilson/database/mysql/20140430140500_mtwilson20_unique_aikpublickey.sql
@@ -2,7 +2,7 @@
 -- rksavino
 -- Makes aik_publickey a unique key in mw_hosts, also adds a column for aik_publickey_sha1
 
-ALTER TABLE `mw_as`.`mw_hosts` ADD UNIQUE KEY `mw_host_aik_public_key_unique` (`aik_publickey`);
+-- ALTER TABLE `mw_as`.`mw_hosts` ADD UNIQUE KEY `mw_host_aik_public_key_unique` (`aik_publickey`);
 ALTER TABLE `mw_as`.`mw_hosts` ADD COLUMN `aik_publickey_sha1` varchar(40) DEFAULT NULL AFTER `aik_publickey`;
   
 INSERT INTO `mw_changelog` (`ID`, `APPLIED_AT`, `DESCRIPTION`) VALUES (20140430140500,NOW(),'Patch to make aik_publickey a unique key in mw_hosts, also adds a column for aik_publickey_sha1.');
