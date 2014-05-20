a118bed14eee6adbdd59c54d645edb476e7a13af
Jonathan Buhacoff <jonathan.buhacoff@intel.com>
2014-05-20 10:54:52 -0700

    dev-2.0-sprint-3-selectionxml: fix for provisioning certificates with default selections

--- services/mtwilson-tag-ws-v2/src/main/java/com/intel/mtwilson/tag/TagCertificateAuthority.java
+++ services/mtwilson-tag-ws-v2/src/main/java/com/intel/mtwilson/tag/TagCertificateAuthority.java
@@ -79,6 +79,22 @@ public class TagCertificateAuthority {
             }
             valid.getSelection().add(selection);
         }
+        // repeat the same action for the default selections
+        if( selections.getDefault() != null ) {
+            valid.setDefault(new DefaultType());
+            for (SelectionType selection : selections.getDefault().getSelection()) {
+                // skip if the selection is not currently valid (notBefore<today<notAfter) ; if notBefore or notAfter are not defined, then validity is assumed
+                if (selection.getNotBefore() != null && today.before(selection.getNotBefore().toGregorianCalendar())) {
+                    log.debug("skipping selection because of notBefore date");
+                    continue;
+                }
+                if (selection.getNotAfter() != null && today.after(selection.getNotAfter().toGregorianCalendar())) {
+                    log.debug("skipping selection because of notAfter date");
+                    continue;
+                }
+                valid.getDefault().getSelection().add(selection);
+            }
+        }
         return valid;
     }
 
@@ -196,12 +212,11 @@ public class TagCertificateAuthority {
                 }
             }
         }
-        // third search for default selection (no subjects declared)  
+        // third search for default selection (no subjects declared) return the first one found
         if( currentSelections.getDefault() != null ) {
             for (SelectionType selection : currentSelections.getDefault().getSelection()) {
-                if (selection.getSubject().isEmpty()) {
-                    return getInlineOrLookupSelection(selection);
-                }
+                log.debug("Using first currently valid default selection");
+                return getInlineOrLookupSelection(selection); // get first default
             }
         }
         /*
