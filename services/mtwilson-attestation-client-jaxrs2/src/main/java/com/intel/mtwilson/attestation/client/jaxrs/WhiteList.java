a414c20efdd096af51543c05eec7a23f6407313c
Sudhir S Bangalore <sudhir.s.bangalore@intel.com>
2014-07-24 22:15:05 +0530

    dev-2.1-sprint-5: Updated the white list RPC documentation for the Tls policy updates.

--- services/mtwilson-attestation-client-jaxrs2/src/main/java/com/intel/mtwilson/attestation/client/jaxrs/WhiteList.java
+++ services/mtwilson-attestation-client-jaxrs2/src/main/java/com/intel/mtwilson/attestation/client/jaxrs/WhiteList.java
@@ -33,7 +33,9 @@ public class WhiteList extends MtWilsonClient {
      * all the information retrieved from the host, OEM, OS and MLEs (both BIOS and VMM) are configured automatically. <br>
      * For Open Source (Xen/KVM) & Citrix XenServer hosts, PCRs 0, 17 & 18 are selected by default. For VMware ESXi hosts, PCRs
      * 0, 17, 18, 19 & 20 are selected by default. The default white list target for both BIOS and VMM would be set to OEM. <br>
-     * If the user wants to change any of the default selections, then custom white list API should be used.     
+     * If the user wants to change any of the default selections, then custom white list API should be used.   
+     * Here we are assuming that the user already created the TlsPolicy to be used to connect to the host. Alternatively, the user
+     * can also specify the TlsPolicy details using the TlsPolicyDescriptor without having to pre-configure the TlsPolicy. 
      * @param CreateWhiteListRpcInput object having the IP address and the connection string of the host from which the white list has
      * to be created.
      * @return boolean value indicating whether the whitelist was successfully created or not.
@@ -44,21 +46,22 @@ public class WhiteList extends MtWilsonClient {
      * @mtwSampleRestCall
      * <pre>
      * https://server.com:8443/mtwilson/v2/rpc/create-whitelist
-     * Input: {"host":{"host_name":"192.168.0.2","add_on_connection_string":"vmware:https://192.168.0.1:443/sdk;admin;pwd"}}
-     * Output: {"host":{"host_name":"10.1.71.155","ipaddress":null,"port":null,"bios_name":"Intel_Corporation",
-     * "bios_version":"01.00.0060","bios_oem":"Intel Corporation","vmm_name":"Intel_Thurley_VMware_ESXi",
-     * "vmm_version":"5.1.0-799733","vmm_osname":"VMware_ESXi","vmm_osversion":"5.1.0",
-     * "add_on_connection_string":"vmware:https://10.1.71.87:443/sdk;Administrator;P@ssw0rd","description":null,
-     * "email":null,"location":null,"aik_certificate":null,"aik_public_key":null,"aik_sha1":null,"processor_info":"Westmere"},
-     * "result":"true"}
+     * Input: {"host":{"host_name":"192.168.0.2","add_on_connection_string":"vmware:https://192.168.0.1:443/sdk;admin;pwd",
+     *          "tls_policy_choice":{"tls_policy_id":"e1a527b5-2020-49c1-83be-6bd8bf641258"}}}
+     * Output: {"host":{"host_name":"192.168.0.2","bios_name":"Intel_Corporation","bios_version":"01.00.0060","bios_oem":"Intel Corporation",
+     *          "vmm_name":"Intel_Thurley_VMware_ESXi","vmm_version":"5.1.0-799733","vmm_osname":"VMware_ESXi","vmm_osversion":"5.1.0",
+     *          "add_on_connection_string":"vmware:https://192.168.0.1:443/sdk;admin;password","processor_info":"Westmere",
+     *          "tls_policy_choice":{"tls_policy_id":"e1a527b5-2020-49c1-83be-6bd8bf641258"}},"result":"true"}
      * </pre>
      * @mtwSampleApiCall
      * <pre>
-     *  Properties prop = My.configuration().getClientProperties();
-     *  WhiteList client = new WhiteList(prop);
+     *  WhiteList client = new WhiteList(My.configuration().getClientProperties());
      *  TxtHostRecord gkvHost = new TxtHostRecord();
-     *  gkvHost.HostName = "192.168.0.10";
+     *  gkvHost.HostName = "192.168.0.2";
      *  gkvHost.AddOn_Connection_String = "vmware:https://192.168.0.1:443/sdk;admin;pwd";
+     *  TlsPolicyChoice tlsPolicyChoice = new TlsPolicyChoice();
+     *  tlsPolicyChoice.setTlsPolicyId("e1a527b5-2020-49c1-83be-6bd8bf641258");
+     *  gkvHost.tlsPolicyChoice = tlsPolicyChoice;
      *  CreateWhiteListRpcInput rpcInput = new CreateWhiteListRpcInput();
      *  rpcInput.setHost(gkvHost);        
      *  boolean result = client.createWhitelist(rpcInput);
@@ -95,6 +98,8 @@ public class WhiteList extends MtWilsonClient {
      * new tBoot version etc), then a new MLE will be created with a numeric extension (_001, 002).
      * Users can also specify any custom name that they want to use for the MLE names. If none is provided, then the default name
      * would be created by the system using the OEM/OS names and version.
+     * Here we are assuming that the user already created the TlsPolicy to be used to connect to the host. Alternatively, the user
+     * can also specify the TlsPolicy details using the TlsPolicyDescriptor without having to pre-configure the TlsPolicy. 
      * @param CreateWhiteListWithOptionsRpcInput object with the host details that would be used to create the white list and the
      * customization options.
      * @return boolean value indicating whether the whitelist was successfully created or not.
@@ -107,25 +112,28 @@ public class WhiteList extends MtWilsonClient {
      * <pre>
      * https://server.com:8443/mtwilson/v2/rpc/create-whitelist-with-options
      * Input: {"wl_config":{"add_bios_white_list":true,"add_vmm_white_list":true,"bios_white_list_target":"BIOS_OEM","vmm_white_list_target":"VMM_OEM",
-     * "bios_pcrs":"0,17","vmm_pcrs":"18,19,20","register_host":true,"overwrite_whitelist":false,"bios_mle_name":"Custom_BIOS_Name","vmm_mle_name":"Custom_VMM_Name"
-     * "txt_host_record":{"host_name":"192.168.0.2","add_on_connection_string":"vmware:https://192.168.0.1:443/sdk;admin;pwd"}}}
+     *          "bios_pcrs":"0,17","vmm_pcrs":"18,19,20","register_host":true,"overwrite_whitelist":false,"bios_mle_name":"Custom_BIOS_Name",
+     *          "vmm_mle_name":"Custom_VMM_Name","txt_host_record":{"host_name":"192.168.0.2",
+     *          "add_on_connection_string":"vmware:https://192.168.0.1:443/sdk;admin;pwd","tls_policy_choice":{"tls_policy_id":"e1a527b5-2020-49c1-83be-6bd8bf641258"}}}}
      * 
      * Output: {"wl_config":{"bios_mle_name":"Custom_BIOS_Name","vmm_mle_name":"Custom_VMM_Name","add_bios_white_list":true,"add_vmm_white_list":true,
-     * "bios_white_list_target":"BIOS_OEM","vmm_white_list_target":"VMM_OEM","bios_pcrs":"0,17","vmm_pcrs":"18,19,20","host_location":"",
-     * "register_host":true,"host_vmm_type":null,"txt_host_record":{"host_name":"192.168.0.2","ipaddress":null,"port":null,"bios_name":"Intel_Corporation",
-     * "bios_version":"01.00.0060","bios_oem":"Intel Corporation","vmm_name":"Intel_Thurley_VMware_ESXi","vmm_version":"5.1.0-799733",
-     * "vmm_osname":"VMware_ESXi","vmm_osversion":"5.1.0","add_on_connection_string":"vmware:https://192.168.0.1:443/sdk;admin;pwd",
-     * "description":null,"email":null,"location":null,"aik_certificate":null,"aik_public_key":null,"aik_sha1":null,"processor_info":"Westmere"},
-     * "overwrite_whitelist":false},"result":"true"}
+     *          "bios_white_list_target":"BIOS_OEM","vmm_white_list_target":"VMM_OEM","bios_pcrs":"0,17","vmm_pcrs":"18,19,20","host_location":"",
+     *          "register_host":true,"host_vmm_type":null,"txt_host_record":{"host_name":"192.168.0.2","ipaddress":null,"port":null,"bios_name":"Intel_Corporation",
+     *          "bios_version":"01.00.0060","bios_oem":"Intel Corporation","vmm_name":"Intel_Thurley_VMware_ESXi","vmm_version":"5.1.0-799733",
+     *          "vmm_osname":"VMware_ESXi","vmm_osversion":"5.1.0","add_on_connection_string":"vmware:https://192.168.0.1:443/sdk;admin;pwd",
+     *          "tls_policy_choice":{"tls_policy_id":"e1a527b5-2020-49c1-83be-6bd8bf641258","description":null,"email":null,"location":null,
+     *          "aik_certificate":null,"aik_public_key":null,"aik_sha1":null,"processor_info":"Westmere"},"overwrite_whitelist":false},"result":"true"}
      * 
      * </pre>
      * @mtwSampleApiCall
      * <pre>
-     *  Properties prop = My.configuration().getClientProperties();
-     *  WhiteList client = new WhiteList(prop);
+     *  WhiteList client = new WhiteList(My.configuration().getClientProperties());
      *  TxtHostRecord gkvHost = new TxtHostRecord();
      *  gkvHost.HostName = "192.168.0.10";
      *  gkvHost.AddOn_Connection_String = "vmware:https://192.168.0.1:443/sdk;admin;pwd";
+     *  TlsPolicyChoice tlsPolicyChoice = new TlsPolicyChoice();
+     *  tlsPolicyChoice.setTlsPolicyId("e1a527b5-2020-49c1-83be-6bd8bf641258");
+     *  gkvHost.tlsPolicyChoice = tlsPolicyChoice;
      *  WhitelistConfigurationData config = new WhitelistConfigurationData();
      *  config.setBiosWhiteList(true);
      *  config.setVmmWhiteList(true);
