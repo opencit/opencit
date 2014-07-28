eaf668a58174e091fc5d1c241b761a4c1e00279f
Sudhir S Bangalore <sudhir.s.bangalore@intel.com>
2014-07-28 09:01:05 +0530

    dev-2.1-sprint-5: Bug fixes in the HostAik and HostAikCertificate V2 APIs.

--- services/mtwilson-attestation-model/src/main/java/com/intel/mtwilson/as/rest/v2/model/HostAikFilterCriteria.java
+++ services/mtwilson-attestation-model/src/main/java/com/intel/mtwilson/as/rest/v2/model/HostAikFilterCriteria.java
@@ -6,6 +6,7 @@ package com.intel.mtwilson.as.rest.v2.model;
 
 import com.intel.mtwilson.repository.FilterCriteria;
 import com.intel.mtwilson.jaxrs2.DefaultFilterCriteria;
+import java.util.UUID;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.QueryParam;
 
@@ -16,7 +17,7 @@ import javax.ws.rs.QueryParam;
 public class HostAikFilterCriteria extends DefaultFilterCriteria implements FilterCriteria<HostAik> {
     
     @PathParam("host_id")
-    public String hostUuid;
+    public UUID hostUuid;
     @QueryParam("id")
     public String aikSha1;
     
