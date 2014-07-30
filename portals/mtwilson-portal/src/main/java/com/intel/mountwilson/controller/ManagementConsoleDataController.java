809eda8361fb9dca2621672cd8539ae49f62e237
Boski <boski.shah@intel.com>
2014-07-29 17:52:53 -0700

    dev-2.1-sprint-5: Klocwork fix 665 - Throwing an exception for null value

--- portals/mtwilson-portal/src/main/java/com/intel/mountwilson/controller/ManagementConsoleDataController.java
+++ portals/mtwilson-portal/src/main/java/com/intel/mountwilson/controller/ManagementConsoleDataController.java
@@ -2872,12 +2872,14 @@ public class ManagementConsoleDataController extends MultiActionController {
         
         try {
             ApiClient apiClient = getApiClientService(req, ApiClient.class);
-            if(apiClient != null){
-                apiClient.setLocale(LocaleUtil.forLanguageTag(locale));
-                HttpSession session = req.getSession();
-                session.setAttribute("api-object", apiClient);
-                session.setAttribute("apiClientObject",apiClient);
+            if (apiClient == null) {
+                throw new IllegalStateException("Failed to initialize the API client object.");
             }
+            apiClient.setLocale(LocaleUtil.forLanguageTag(locale));
+            HttpSession session = req.getSession();
+            session.setAttribute("api-object", apiClient);
+            session.setAttribute("apiClientObject",apiClient);
+            
             responseView.addObject("locale", demoPortalServices.setLocale(username, locale, apiClient));
         } catch (DemoPortalException e) {
             e.printStackTrace();
