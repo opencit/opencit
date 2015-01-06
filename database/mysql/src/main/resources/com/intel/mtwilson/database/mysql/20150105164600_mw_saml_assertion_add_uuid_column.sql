1e7e96bb89e43f895e47a7e77910985b600539c2
Ryan Savino <ryan.k.savino@intel.com>
2015-01-05 17:04:42 -0800

    dev-2.0.3-sprint-3-bug-2165-host-attestations: added sql scripts to add uuid field to mw_saml_assertion

---
+++ database/mysql/src/main/resources/com/intel/mtwilson/database/mysql/20150105164600_mw_saml_assertion_add_uuid_column.sql
@@ -0,0 +1,6 @@
+-- rksavino
+-- Updates for the mw_saml_assertion table
+ALTER TABLE `mw_saml_assertion` ADD COLUMN `uuid_hex` CHAR(36) NULL;
+UPDATE mw_saml_assertion SET uuid_hex = (SELECT uuid());
+
+INSERT INTO changelog (ID, APPLIED_AT, DESCRIPTION) VALUES (20150105164600,NOW(),'Added UUID column for mw_saml_assertion');
