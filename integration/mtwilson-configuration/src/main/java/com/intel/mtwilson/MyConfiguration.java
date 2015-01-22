0bb03fdb6f69e824b3dd202e9b2a4ce8264c68be
boski <boski.shah@intel.com>
2015-01-21 16:40:19 -0800

    dev-3.x-mtwilson-vm-merge0121: Trust Policy Signature API changes

--- integration/mtwilson-configuration/src/main/java/com/intel/mtwilson/MyConfiguration.java
+++ integration/mtwilson-configuration/src/main/java/com/intel/mtwilson/MyConfiguration.java
@@ -699,6 +699,10 @@ public class MyConfiguration {
     public String getSamlKeystorePassword() {
         return conf.getString("saml.key.password"); // bug #733 
     }
+    
+    public String getSamlKeyAlias() {
+        return conf.getString("saml.key.alias"); 
+    }
 
     ///////////////////////// tls policy  //////////////////////////////////
     public String getGlobalTlsPolicyId() {
