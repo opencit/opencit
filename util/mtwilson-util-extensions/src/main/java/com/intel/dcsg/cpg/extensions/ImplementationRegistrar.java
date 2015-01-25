060b88ff8214471f51ae5b5b6b159bd20a8d11e4
Jonathan Buhacoff <jonathan.buhacoff@intel.com>
2015-01-24 22:44:41 -0800

    dev-3.x-jb-launcher: package includes/excludes are now in Scanner so they can apply to all registrars

--- util/mtwilson-util-extensions/src/main/java/com/intel/dcsg/cpg/extensions/ImplementationRegistrar.java
+++ util/mtwilson-util-extensions/src/main/java/com/intel/dcsg/cpg/extensions/ImplementationRegistrar.java
@@ -31,8 +31,6 @@ public class ImplementationRegistrar implements Registrar {
 
     private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(ImplementationRegistrar.class);
     private final List<String> acceptable; // when null, will register ANY implementation of ANY interface; when set, will only register specific interfaces
-    private List<String> includePackages = null; // when null, will accept implementations in any package; when set, will only accept implementations in specified packages
-    private List<String> excludePackages = null; // when null, will accept implementations in any package; when set, will exclude implementations in specified packages (overrides includePackages so can be used to exclude specific portion of included package)
 
     public ImplementationRegistrar() {
         this.acceptable = null;
@@ -43,14 +41,6 @@ public class ImplementationRegistrar implements Registrar {
         for (Class<?> clazz : interfaces) {
             acceptable.add(clazz.getName());
         }
-    }
-
-    public void setIncludePackages(List<String> includePackages) {
-        this.includePackages = includePackages;
-    }
-
-    public void setExcludePackages(List<String> excludePackages) {
-        this.excludePackages = excludePackages;
     }
 
     @Override
@@ -84,36 +74,13 @@ public class ImplementationRegistrar implements Registrar {
 
     private boolean register(Class<?> clazz, Class<?>... interfaces) {
         boolean accepted = false;
-        if (includePackages == null || startsWithAny(clazz.getName(), toPackagePrefixes(includePackages))) {
-            if (excludePackages == null || !startsWithAny(clazz.getName(), toPackagePrefixes(excludePackages))) {
                 for (int i = 0; i < interfaces.length; i++) {
                     if (acceptable == null || acceptable.contains(interfaces[i].getName())) {
                         WhiteboardExtensionProvider.register(interfaces[i], clazz);
                         accepted = true;
                     }
                 }
-            }
-        }
         return accepted;
     }
 
-    private boolean startsWithAny(String test, List<String> prefixes) {
-        for (String prefix : prefixes) {
-            if (test.startsWith(prefix)) {
-                return true;
-            }
-        }
-        return false;
-    }
-    
-    // the prefixes are like "java", "javax", "com.intel", etc.
-    // we return the same prefixes with a "." at the end so they become
-    // "java.", "javax.", "com.intel.", etc. 
-    private List<String> toPackagePrefixes(List<String> prefixes) {
-        ArrayList<String> packagePrefixes = new ArrayList<>();
-        for(String prefix : prefixes) {
-            packagePrefixes.add(prefix+".");
-        }
-        return packagePrefixes;
-    }
 }
