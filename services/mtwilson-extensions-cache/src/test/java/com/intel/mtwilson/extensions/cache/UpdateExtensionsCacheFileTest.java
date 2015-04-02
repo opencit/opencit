fd540c36df256f6b48833df7dd6a051b5c6feb4a
Jonathan Buhacoff <jonathan.buhacoff@intel.com>
2015-04-02 08:05:02 -0700

    dev-3.x: added option to UpdateExtensionsCacheFile to limit scan to files matching at least one specified keyword in the filename using the configuration setting mtwilson.extensions.fileIncludeFilter.contains which can be a comma-separated list

--- services/mtwilson-extensions-cache/src/test/java/com/intel/mtwilson/extensions/cache/UpdateExtensionsCacheFileTest.java
+++ services/mtwilson-extensions-cache/src/test/java/com/intel/mtwilson/extensions/cache/UpdateExtensionsCacheFileTest.java
@@ -4,8 +4,10 @@
  */
 package com.intel.mtwilson.extensions.cache;
 
+import com.intel.dcsg.cpg.configuration.PropertiesConfiguration;
 import com.intel.dcsg.cpg.validation.Fault;
 import java.io.File;
+import java.io.FileFilter;
 import java.util.List;
 import org.junit.Test;
 
@@ -30,6 +32,56 @@ public class UpdateExtensionsCacheFileTest {
         }
     }
 
+    @Test
+    public void testUpdateExtensionsCacheFileWithFilterInstance() throws Exception {
+        File cacheFile = new File("target"+File.separator+"extensions.cache");
+        UpdateExtensionsCacheFile task = new UpdateExtensionsCacheFile();
+        task.setCacheFile(cacheFile);
+        task.setFileIncludeFilter(new AppJarsFilter());
+        if( task.isConfigured() ) {
+            log.debug("configured ok");
+            task.execute();
+        }
+        else {
+            printFaults(task.getConfigurationFaults());
+        }
+    }
+    
+    @Test
+    public void testUpdateExtensionsCacheFileWithFilterClassName() throws Exception {
+        File cacheFile = new File("target"+File.separator+"extensions.cache");
+        UpdateExtensionsCacheFile task = new UpdateExtensionsCacheFile();
+        task.setCacheFile(cacheFile);
+        PropertiesConfiguration configuration = new PropertiesConfiguration();
+        configuration.set("mtwilson.extensions.fileIncludeFilter.class", "com.intel.mtwilson.extensions.cache.AppJarsFilter");
+        task.setConfiguration(configuration);
+        if( task.isConfigured() ) {
+            log.debug("configured ok");
+            task.execute();
+        }
+        else {
+            printFaults(task.getConfigurationFaults());
+        }
+    }
+    
+    @Test
+    public void testUpdateExtensionsCacheFileWithFilterContains() throws Exception {
+        File cacheFile = new File("target"+File.separator+"extensions.cache");
+        UpdateExtensionsCacheFile task = new UpdateExtensionsCacheFile();
+        task.setCacheFile(cacheFile);
+        PropertiesConfiguration configuration = new PropertiesConfiguration();
+        configuration.set("mtwilson.extensions.fileIncludeFilter.contains", "mtwilson"); // can be comma-separated list
+        task.setConfiguration(configuration);
+        if( task.isConfigured() ) {
+            log.debug("configured ok");
+            task.execute();
+        }
+        else {
+            printFaults(task.getConfigurationFaults());
+        }
+    }
+    
+    
     @Test
     public void testUpdateExtensionsCacheFileValidation() throws Exception {
         File cacheFile = new File("target"+File.separator+"extensions.cache");
