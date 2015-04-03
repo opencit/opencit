fb8fc090be9b077242aed9bc638f4742b6c9682f
Sudhir S Bangalore <sudhir.s.bangalore@intel.com>
2015-04-04 00:19:06 +0530

    dev-3.x-ta-vrtm-java-client: Few more updates to the junit test.

--- trust-agent/mtwilson-trustagent-ws-v2/src/test/java/test/VMQuote/VMQuoteTest.java
+++ trust-agent/mtwilson-trustagent-ws-v2/src/test/java/test/VMQuote/VMQuoteTest.java
@@ -9,6 +9,9 @@ import org.junit.Test;
 import com.intel.mtwilson.vmquote.xml.*;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
+import javax.xml.bind.JAXBException;
+import javax.xml.stream.XMLStreamException;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 
@@ -30,6 +33,19 @@ public class VMQuoteTest {
         
         JAXB jaxb = new JAXB();
         
+        try {
+            VMQuote vmInstanceQuote = new VMQuote();
+            vmInstanceQuote.setCumulativeHash("2284377e7a81243ab4305412669d90ba9253a64a");
+            vmInstanceQuote.setVmInstanceId("efc8ac4b-bd45-49d2-880c-bdd64feeafd4");
+            vmInstanceQuote.setDigestAlg("SHA-256");
+            vmInstanceQuote.setNonce("abc8ac4b-bd45-49d2-880c-bdd64feeafd6");
+            
+            vmQuoteResponse.setVMQuote(vmInstanceQuote);
+            
+        } catch (Exception ex) {
+            log.error("Error reading the vm quote file. {}", ex.getMessage());
+        }
+        
         try (FileInputStream measurementXMLFileStream = new FileInputStream(String.format("%s%s", instanceFolderPath, measurementXMLFileName))) {
         
             String measurementXML = IOUtils.toString(measurementXMLFileStream, "UTF-8");
@@ -40,9 +56,12 @@ public class VMQuoteTest {
             log.error("Error reading the measurement log. {}", ex.getMessage());
         }
         
-        try (FileInputStream signingKeyPemFileStream = new FileInputStream(String.format("%s%s", instanceFolderPath, signingKeyPemFileName))) {
+//        try (FileInputStream signingKeyPemFileStream = new FileInputStream(String.format("%s%s", instanceFolderPath, signingKeyPemFileName))) {
+        try {
             
-            String signingKey = IOUtils.toString(signingKeyPemFileStream, "UTF-8");
+            File signingKeyPemFile = new File(String.format("%s%s", instanceFolderPath, signingKeyPemFileName));
+            //String signingKey = IOUtils.toString(signingKeyPemFileStream, "UTF-8");
+            String signingKey = FileUtils.readFileToString(signingKeyPemFile);
             vmQuoteResponse.setSigningKeyCertificate(signingKey);
             
         } catch (Exception ex) {
@@ -60,9 +79,22 @@ public class VMQuoteTest {
             log.error("Error reading the measurement log. {}", ex.getMessage());
         }
 
-        String writeMeasurements = jaxb.write(vmQuoteResponse);
-        log.debug(writeMeasurements);
+        String quoteResponse = jaxb.write(vmQuoteResponse);
+        FileUtils.write(new File(instanceFolderPath + "outputForMTW.xml"), quoteResponse);
+        log.debug(quoteResponse);        
+    }  
+    
+    @Test
+    public void verifyVMQuoteResponse() {
+        
+        JAXB jaxb = new JAXB();
+        try {
+            VMQuoteResponse vmQuoteResponse = jaxb.read(FileUtils.readFileToString(new File(instanceFolderPath + "outputForMTW.xml")), VMQuoteResponse.class);
+            log.debug(vmQuoteResponse.getSigningKeyCertificate());
+            log.debug(vmQuoteResponse.getVMQuote().getCumulativeHash());
+        } catch(IOException | JAXBException | XMLStreamException ex) {
+            log.error("Error during deserialization of VM Quote Response. {}", ex.getMessage());
+        }
         
     }
-    
 }
