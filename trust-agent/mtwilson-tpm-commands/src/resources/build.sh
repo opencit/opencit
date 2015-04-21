02d3aadfd768817cdd91f754c955834bb673d357
Ryan Savino <ryan.k.savino@intel.com>
2015-04-21 13:44:30 -0700

    dev-3.x: added tpm_signdata and tpm_createkey gcc build commands to mtwilson-tpm-commands/src/resources/build.sh added tpm_signdata and tpm_createkey to mtwilson-tpm-commands

--- trust-agent/mtwilson-tpm-commands/src/resources/build.sh
+++ trust-agent/mtwilson-tpm-commands/src/resources/build.sh
@@ -51,3 +51,5 @@ install_tpm_tools
 #gcc  -lcrypto -ltspi -o target/create_tpm_key2 create_tpm_key2.c
 gcc -g -O0 -DLOCALEDIR='"/usr/share/locale"' -Itpm-tools-1.3.8 -Itpm-tools-1.3.8/include -o tpm_bindaeskey tpm_bindaeskey.c tpm-tools-1.3.8/lib/tpm_tspi.c tpm-tools-1.3.8/lib/tpm_utils.c tpm-tools-1.3.8/lib/tpm_log.c -lcrypto -ltspi
 gcc -g -O0 -DLOCALEDIR='"/usr/share/locale"' -Itpm-tools-1.3.8 -Itpm-tools-1.3.8/include -o tpm_unbindaeskey tpm_unbindaeskey.c tpm-tools-1.3.8/lib/tpm_tspi.c tpm-tools-1.3.8/lib/tpm_utils.c tpm-tools-1.3.8/lib/tpm_log.c hex2bytea.c -lcrypto -ltspi
+gcc -g -O0 -DLOCALEDIR='"/usr/share/locale"' -Itpm-tools-1.3.8 -Itpm-tools-1.3.8/include -o tpm_signdata  tpm_signdata.c tpm-tools-1.3.8/lib/tpm_tspi.c tpm-tools-1.3.8/lib/tpm_utils.c tpm-tools-1.3.8/lib/tpm_log.c hex2bytea.c -lcrypto –ltspi
+gcc -g -O0 -DLOCALEDIR='"/usr/share/locale"' -Itpm-tools-1.3.8 -Itpm-tools-1.3.8/include -o tpm_createkey tpm_createkey.c tpm-tools-1.3.8/lib/tpm_tspi.c tpm-tools-1.3.8/lib/tpm_utils.c tpm-tools-1.3.8/lib/tpm_log.c hex2bytea.c -lcrypto –ltspi
