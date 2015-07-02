d0082856bd0740b6595c3485af4fcab92d9fac8f
Ryan Savino <ryan.k.savino@intel.com>
2015-07-01 21:13:45 -0700

    release-2.0.6: fixed tomcat_detect function; added initial check for TOMCAT_HOME in MTWILSON_HOME location

--- installers/LinuxUtil/src/files/functions.sh
+++ installers/LinuxUtil/src/files/functions.sh
@@ -2830,46 +2830,43 @@ tomcat_detect() {
 
   if [[ (-z $JAVA_HOME && -z $JRE_HOME) || -z $java ]]; then java_detect; fi
   if [[ (-z $JAVA_HOME && -z $JRE_HOME) || -z $java ]]; then return 1; fi
-
-      if [[ -n "$java" ]]; then    
-        local java_bindir=`dirname $java`
-      fi
+  if [[ -n "$java" ]]; then    
+    local java_bindir=`dirname $java`
+  fi
 
   # start with TOMCAT_HOME if it is already configured
   if [ "$(whoami)" == "root" ]; then
-   if [[ -n "$TOMCAT_HOME" ]]; then
-     if [[ -z "$tomcat_bin" ]]; then
-       tomcat_bin="$TOMCAT_HOME/bin/catalina.sh"
-     fi
-     if [[ -z "$tomcat" ]]; then
-       if [[ -n "$java" ]]; then    
-         # the glassfish admin tool read timeout is in milliseconds, so 900,000 is 900 seconds
-         tomcat="env PATH=$java_bindir:$PATH $tomcat_bin"
-       else
-         tomcat="$tomcat_bin"
-       fi
-     fi
-     if [ -z "$TOMCAT_CONF" ]; then
-       if [ -d "$TOMCAT_HOME/conf" ] && [ -f "$TOMCAT_HOME/conf/tomcat-users.xml" ] && [ -f "$TOMCAT_HOME/conf/server.xml" ]; then
-         export TOMCAT_CONF="$TOMCAT_HOME/conf"
-       else
-         # we think we know TOMCAT_HOME but we can't find TOMCAT_CONF so
-         # reset the "tomcat" variable to force a new detection below
-         tomcat=""
-       fi
-     fi
-     if [[ -n "$tomcat" ]]; then
-       #TOMCAT_VERSION=`tomcat_version`
-       tomcat_version
-       if is_version_at_least "$TOMCAT_VERSION" "${min_version}"; then
-         return 0
-       fi
-     fi
-   fi
-   searchdir=/
+    if [ -n "$TOMCAT_HOME" ] && [[ "$TOMCAT_HOME" == /opt/mtwilson* ]]; then
+      if [ -z "$tomcat_bin" ]; then
+        tomcat_bin="$TOMCAT_HOME/bin/catalina.sh"
+      fi
+      if [ -z "$tomcat" ]; then
+        if [ -n "$java" ]; then    
+          # the glassfish admin tool read timeout is in milliseconds, so 900,000 is 900 seconds
+          tomcat="env PATH=$java_bindir:$PATH $tomcat_bin"
+        else
+          tomcat="$tomcat_bin"
+        fi
+      fi
+      if [ -d "$TOMCAT_HOME/conf" ] && [ -f "$TOMCAT_HOME/conf/tomcat-users.xml" ] && [ -f "$TOMCAT_HOME/conf/server.xml" ]; then
+        export TOMCAT_CONF="$TOMCAT_HOME/conf"
+      else
+        # we think we know TOMCAT_HOME but we can't find TOMCAT_CONF so
+        # reset the "tomcat" variable to force a new detection below
+        tomcat=""
+      fi
+      if [ -n "$tomcat" ]; then
+        #TOMCAT_VERSION=`tomcat_version`
+        tomcat_version
+        if is_version_at_least "$TOMCAT_VERSION" "${min_version}"; then
+          return 0
+        fi
+      fi
+    fi
+    searchdir=/
   else
-  #TODO update it to $MTWILSON_HOME
-   searchdir=/opt/mtwilson
+    #TODO update it to $MTWILSON_HOME
+    searchdir=/opt/mtwilson
   fi
   
   #echo "tomcat variable is $tomcat"
@@ -2881,8 +2878,7 @@ tomcat_detect() {
   fi
   tomcat_clear
   echo "debug TOMCAT_CANDIDATES: ${TOMCAT_CANDIDATES}" >> $INSTALL_LOG_FILE
-  for c in $TOMCAT_CANDIDATES
-  do
+  for c in $TOMCAT_CANDIDATES; do
     #echo "debug tomcat candidate: $c"
     if [ -z "$TOMCAT_HOME" ]; then
       local conf_dir=`dirname $c`
