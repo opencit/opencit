aa5ca329a89abd061c658d88b806d17f4d2c90b3
Haidong Xia <haidong.xia@intel.com>
2015-07-06 11:22:55 -0700

    dev-3.x-windows: MTWilson UI update: added Windows host type and required PCRs in HostVMMType.java and updated the WhiteListConfiguration.jsp to have the option to choose Windows

--- integration/mtwilson-api/src/main/java/com/intel/mtwilson/datatypes/HostVMMType.java
+++ integration/mtwilson-api/src/main/java/com/intel/mtwilson/datatypes/HostVMMType.java
@@ -13,7 +13,8 @@ public enum HostVMMType {
     CITRIX_XENSERVER("Citrix XenServer","0,17,18"),
     KVM("KVM","0,17,18"),
     VMWARE("VMware ESXi","0,17,18,19,20"),
-    XEN("Xen","0,17,18");
+    XEN("Xen","0,17,18"),
+    WINDOWS("WINDOWS", "0,12,13,14");
     
     private String value;
     private String pcrs;
