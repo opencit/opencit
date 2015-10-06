045d4e4eed534ab3e5c00f6f52da6aca19ef6a07
Sudhir S Bangalore <sudhir.s.bangalore@intel.com>
2015-10-06 16:35:25 +0530

    dev-3.x-Bug-4374: Updates to IntelHostAgentFactory to handle login id & password. If not provided by user then search for it in the pre-registration table.

--- services/mtwilson-intel-hostagent/src/main/java/com/intel/mtwilson/agent/intel/IntelHostAgentFactory.java
+++ services/mtwilson-intel-hostagent/src/main/java/com/intel/mtwilson/agent/intel/IntelHostAgentFactory.java
@@ -9,6 +9,8 @@ import com.intel.mtwilson.agent.VendorHostAgentFactory;
 import com.intel.mtwilson.model.InternetAddress;
 import com.intel.dcsg.cpg.tls.policy.TlsConnection;
 import com.intel.dcsg.cpg.tls.policy.TlsPolicy;
+import com.intel.mtwilson.My;
+import com.intel.mtwilson.as.data.MwHostPreRegistrationDetails;
 import com.intel.mtwilson.datatypes.ConnectionString;
 import com.intel.mtwilson.trustagent.client.jaxrs.TrustAgentClient;
 import java.io.IOException;
@@ -25,6 +27,7 @@ import org.slf4j.LoggerFactory;
  */
 public class IntelHostAgentFactory implements VendorHostAgentFactory {
     private Logger log = LoggerFactory.getLogger(getClass());
+    private String intelVendorConnectionString = "";
     
     @Override
     public String getVendorProtocol() { return "intel"; }
@@ -32,19 +35,36 @@ public class IntelHostAgentFactory implements VendorHostAgentFactory {
     @Override
     public HostAgent getHostAgent(InternetAddress hostAddress, String vendorConnectionString, TlsPolicy tlsPolicy) throws IOException {
         try {
-            log.debug("IntelHostAgentFactory getHostAgent connection string: {}", vendorConnectionString);
+            intelVendorConnectionString = vendorConnectionString;
             ConnectionString.IntelConnectionString intelConnectionString = ConnectionString.IntelConnectionString.forURL(vendorConnectionString);
-            log.debug("IntelHostAgentFactory: Complete connection string is {}", intelConnectionString.toString());
-            URL url = new URL(vendorConnectionString);
+            log.debug("IntelHostAgentFactory: Connection string URL is {}", intelConnectionString.toURL());
+            // We need to verify if the user has specified the login id and password for the host. If not, we will check in the pre-register host table.
+            // If it is not even present in that table, we will throw an error.
+            if (intelConnectionString.getUsername() == null || intelConnectionString.getUsername().isEmpty() ||
+                    intelConnectionString.getPassword() == null || intelConnectionString.getPassword().isEmpty()) {
+                log.debug("IntelHostAgentFactory - User name or password not specified. Retrieving from table");
+                MwHostPreRegistrationDetails hostLoginDetails = My.jpa().mwHostPreRegistrationDetails().findByName(intelConnectionString.getHost().toString());
+                if (hostLoginDetails != null) {
+                    ConnectionString tempConnectionString = ConnectionString.forIntel(intelConnectionString.getHost().toString(), intelConnectionString.getPort(), 
+                            hostLoginDetails.getLogin(), hostLoginDetails.getPassword());
+                    // Would be used to return back the modified connection string.
+                    intelVendorConnectionString = tempConnectionString.getConnectionString();
+                    log.debug("IntelHostAgentFactory - URL of new connection string is {}", tempConnectionString.getURL());
+                    intelConnectionString = ConnectionString.IntelConnectionString.forURL(tempConnectionString.getConnectionString());
+                }
+            }
+            URL url = intelConnectionString.toURL();
             if( url.getPort() == 1443 || url.getPath().contains("/v2") ) {
                 // assume trust agent v2
-                log.debug("Creating IntelHostAgent v2 for host {}", hostAddress);
+                log.debug("Creating IntelHostAgent v2 for host {} with URL {}", hostAddress, url);
                 Properties properties = new Properties();
                 // mtwilson version 2.0 beta has authentication support on the trust agent but not yet in the mtwilson portal
                 // so we use this default username and empty password until the mtwilson portal is updated to ask for trust agent
                 // login credentials
-                properties.setProperty("mtwilson.api.username", "mtwilson");
-                properties.setProperty("mtwilson.api.password", "");
+                properties.setProperty("mtwilson.api.username", intelConnectionString.getUsername());
+                properties.setProperty("mtwilson.api.password", intelConnectionString.getPassword());
+//                properties.setProperty("mtwilson.api.username", "mtwilson");
+//                properties.setProperty("mtwilson.api.password", "");
 //                properties.setProperty("mtwilson.api.ssl.policy", "INSECURE");
                 
                 // now add the /v2 path if it's not already there,  to maintain compatibility with the existing UI that only prompts for
@@ -79,5 +99,10 @@ public class IntelHostAgentFactory implements VendorHostAgentFactory {
         catch(Exception e) {
             throw new IOException("Cannot get trust agent client for host connection: "+vendorConnectionString+": "+e.toString(), e);
         }
+    }
+
+    @Override
+    public String getVendorConnectionString() {
+        return intelVendorConnectionString;
     }
 }
