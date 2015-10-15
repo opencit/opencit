13939710d6df91b9d8bcb94ec9eea14b08816b99
Sudhir S Bangalore <sudhir.s.bangalore@intel.com>
2015-10-15 11:27:33 +0530

    dev-3.x-Bug-4374: Removed the task that was creating the default mtwilson user without any password.

--- installers/TrustAgentLinuxInstaller/src/files/setup.sh
+++ installers/TrustAgentLinuxInstaller/src/files/setup.sh
@@ -693,7 +693,10 @@ if [ -z "$TRUSTAGENT_NOSETUP" ]; then
   # create a trustagent username "mtwilson" with no password and all privileges
   # which allows mtwilson to access it until mtwilson UI is updated to allow
   # entering username and password for accessing the trust agent
-  /usr/local/bin/tagent password mtwilson --nopass *:*
+  
+  # Starting with 3.0, we have a separate task that creates a new user name and password per host
+  # So we do not need to create this user without password. This is would address the security issue as well
+  #/usr/local/bin/tagent password mtwilson --nopass *:*
 
   # give tagent a chance to do any other setup (such as the .env file and pcakey)
   # and make sure it's successful before trying to start the trust agent
