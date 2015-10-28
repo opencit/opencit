c702cac8ac338a552c49dafca927e8f9107150cd
boski <boski.shah@intel.com>
2015-10-27 17:58:25 -0700

    dev-cit-3.0-sprint3: Updated method in policy signing client

--- services/mtwilson-vm-attestation-client-jaxrs2/src/test/java/TrustPolicySignatureTest.java
+++ services/mtwilson-vm-attestation-client-jaxrs2/src/test/java/TrustPolicySignatureTest.java
@@ -50,9 +50,8 @@ public class TrustPolicySignatureTest{
     @Test
     public void testTrustPolicySignature() throws Exception{
         String policy = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><TrustPolicy xmlns=\"mtwilson:trustdirector:policy:1.1\"><Director><CustomerId>testId</CustomerId></Director><Image><ImageId>cb59ff02-a645-410c-bd4e-a80c6729329d</ImageId><ImageHash DigestAlg=\"sha1\">995dd5b5df990267bdfab238f5fe5f3a80e24540</ImageHash></Image><LaunchControlPolicy>MeasureAndEnforce</LaunchControlPolicy><Encryption><Key URL=\"uri\">http://10.1.68.42/v1/keys/e5da7472-222b-4000-8b62-9d3a566b2716/transfer</Key><Checksum DigestAlg=\"md5\">9b760e45a66a2923d29df3a70addd5ea</Checksum></Encryption><Whitelist DigestAlg=\"sha1\"></Whitelist></TrustPolicy>";
-        JAXB jaxb = new JAXB();
-        TrustPolicy signedPolicy = client.signTrustPolicy(jaxb.read(policy, TrustPolicy.class));
-        log.debug("Signed Policy Is: "+jaxb.write(signedPolicy));
+        String signedPolicy = client.signTrustPolicy(policy);
+        log.debug("Signed Policy Is: "+signedPolicy);
     }
 
 }
