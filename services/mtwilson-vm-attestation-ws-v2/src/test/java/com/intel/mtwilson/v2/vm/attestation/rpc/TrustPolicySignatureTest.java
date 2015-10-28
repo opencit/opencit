f9b38039e6172e7e218cf2e2daa2b4886c2cb128
Jonathan Buhacoff <jonathan.buhacoff@intel.com>
2015-10-27 20:11:03 -0700

    dev-cit-3.0-sprint3-issue-4711-xmldsig-fix: added jaxb parse step to unit test for comparison

--- services/mtwilson-vm-attestation-ws-v2/src/test/java/com/intel/mtwilson/v2/vm/attestation/rpc/TrustPolicySignatureTest.java
+++ services/mtwilson-vm-attestation-ws-v2/src/test/java/com/intel/mtwilson/v2/vm/attestation/rpc/TrustPolicySignatureTest.java
@@ -6,6 +6,8 @@ package com.intel.mtwilson.v2.vm.attestation.rpc;
 
 import com.intel.dcsg.cpg.crypto.CryptographyException;
 import com.intel.dcsg.cpg.crypto.RsaUtil;
+import com.intel.dcsg.cpg.xml.JAXB;
+import com.intel.mtwilson.trustpolicy.xml.TrustPolicy;
 import com.intel.mtwilson.util.xml.dsig.XmlDsigVerify;
 import java.io.IOException;
 import java.nio.charset.Charset;
@@ -16,9 +18,11 @@ import java.security.NoSuchAlgorithmException;
 import java.security.UnrecoverableEntryException;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
+import javax.xml.bind.JAXBException;
 import javax.xml.crypto.MarshalException;
 import javax.xml.crypto.dsig.XMLSignatureException;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.stream.XMLStreamException;
 import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
 import org.apache.commons.io.IOUtils;
@@ -37,7 +41,7 @@ public class TrustPolicySignatureTest {
     private static String xmlpath = "/trustpolicy/trustpolicy_not_signed.xml";
     
     @Test
-    public void testSignVerifyTrustPolicy() throws NoSuchAlgorithmException, CryptographyException, IOException, InvalidAlgorithmParameterException, KeyStoreException, UnrecoverableEntryException, CertificateException, ParserConfigurationException, SAXException, MarshalException, XMLSignatureException, TransformerConfigurationException, TransformerException {
+    public void testSignVerifyTrustPolicy() throws NoSuchAlgorithmException, CryptographyException, IOException, InvalidAlgorithmParameterException, KeyStoreException, UnrecoverableEntryException, CertificateException, ParserConfigurationException, SAXException, MarshalException, XMLSignatureException, TransformerConfigurationException, TransformerException, JAXBException, XMLStreamException {
         // generate rsa keypair
         KeyPair keypair = RsaUtil.generateRsaKeyPair(keySizeInBits);
         X509Certificate certificate = RsaUtil.generateX509Certificate("TrustDirector", keypair, validityInDays);
@@ -49,6 +53,36 @@ public class TrustPolicySignatureTest {
         // verify xml signature
         boolean isValid = XmlDsigVerify.isValid(signedXml, certificate);
         log.debug("xml signature valid? {}", isValid);
-        
+        assertTrue(isValid);
+        // now jaxb read/write
+        JAXB jaxb = new JAXB();
+        String editedXml = jaxb.write(jaxb.read(signedXml, TrustPolicy.class));
+        log.debug("edited xml: {}", editedXml);
+        boolean editedIsValid = XmlDsigVerify.isValid(editedXml, certificate);
+        log.debug("edited xml valid? {}", editedIsValid);
     }
+    
+    @Test
+    public void testSignVerifyTrustPolicyWithNamespace() throws NoSuchAlgorithmException, CryptographyException, IOException, InvalidAlgorithmParameterException, KeyStoreException, UnrecoverableEntryException, CertificateException, ParserConfigurationException, SAXException, MarshalException, XMLSignatureException, TransformerConfigurationException, TransformerException, JAXBException, XMLStreamException {
+        // generate rsa keypair
+        KeyPair keypair = RsaUtil.generateRsaKeyPair(keySizeInBits);
+        X509Certificate certificate = RsaUtil.generateX509Certificate("TrustDirector", keypair, validityInDays);
+        // load , edit, & sign xml
+        JAXB jaxb = new JAXB();
+        String xml = IOUtils.toString(getClass().getResourceAsStream(xmlpath), Charset.forName("UTF-8"));
+        String editedXml = jaxb.write(jaxb.read(xml, TrustPolicy.class));
+        TrustPolicySignature signer = new TrustPolicySignature();
+        String signedXml = signer.generateDsig(editedXml, keypair.getPrivate(), certificate);
+        log.debug("signed xml: {}", signedXml);
+        // verify xml signature
+        boolean isValid = XmlDsigVerify.isValid(signedXml, certificate);
+        log.debug("xml signature valid? {}", isValid);
+        assertTrue(isValid);
+        // now jaxb read/write again
+        String editedXml2 = jaxb.write(jaxb.read(signedXml, TrustPolicy.class));
+        log.debug("edited xml: {}", editedXml2);
+        boolean editedIsValid = XmlDsigVerify.isValid(editedXml2, certificate);
+        log.debug("edited xml valid? {}", editedIsValid);
+    }
+    
 }
