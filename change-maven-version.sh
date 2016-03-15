74f491ce9092e9529412c9996d0fc4cbff9a0450
Ryan Savino <ryan.k.savino@intel.com>
2016-03-14 18:02:02 -0700

    dev-cit-3.1: integration poms: cleaned up formatting added version and groupId fields to all pom top level updated parent versions removed unnecessary version declarations for dependencies managed under dependencyManagement
    deleted integration/mtwilson-launcher-jersey
    deleted integration/mtwilson-xmlpatch-xml

--- change-maven-version.sh
+++ change-maven-version.sh
@@ -28,4 +28,8 @@ fi
 changeVersionCommand="mvn versions:set -DnewVersion=${version}"
 changeParentVersionCommand="mvn versions:update-parent -DnewVersion=${version}"
 $changeVersionCommand
+if [ $? -ne 0 ]; then echo "Failed to change maven version at top level" >&2; exit 3; fi
+(cd integration && $changeVersionCommand)
+if [ $? -ne 0 ]; then echo "Failed to change maven version on \"integration\" folder" >&2; exit 3; fi
 (cd services && $changeVersionCommand) # && $changeParentVersionCommand)
+if [ $? -ne 0 ]; then echo "Failed to change maven version on \"services\" folder" >&2; exit 3; fi
--- integration/mtwilson-launcher-jersey/src/main/webapp/index.jsp
+++
@@ -1,6 +0,0 @@
-<html>
-<body>
-<h1>Mt Wilson</h1>
-<p><a href="resources/application.wadl">application.wadl</a>
-</body>
-</html>
