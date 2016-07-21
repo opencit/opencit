f9d9966fcfae1dbf7793ec54796b8ad3d8c5af67
Ryan Savino <ryan.k.savino@intel.com>
2016-07-21 10:26:33 -0700

    v2.1+SDL-fixes: removed tpm-tools from attestation service klocwork scan as the real modified version is in contributions repo

--- klocwork.sh
+++ klocwork.sh
@@ -30,8 +30,6 @@ generateBuildSpecs() {
   if [ ! -d "trust-agent/mtwilson-tpm-commands/target" ]; then
     (cd trust-agent/mtwilson-tpm-commands && mvn install)
   fi
-  (cd trust-agent/mtwilson-tpm-commands/target/tpm-tools-1.3.8 && make clean)
-  (cd trust-agent/mtwilson-tpm-commands/target/tpm-tools-1.3.8 && ./configure LDFLAGS="-L/usr/local/lib" --prefix=/usr/local --with-openssl=/usr/local/ssl && kwinject --output "${buildSpecsDirectory}/tpm_tools.out" make)
   (cd trust-agent/mtwilson-tpm-commands/target/hex2bin-master && make clean)
   (cd trust-agent/mtwilson-tpm-commands/target/hex2bin-master && kwinject --output "${buildSpecsDirectory}/hex2bin.out" make)
   (cd trust-agent/mtwilson-tpm-commands/target && make clean)
@@ -39,7 +37,7 @@ generateBuildSpecs() {
 }
 
 buildProject() {
-  kwbuildproject --url "${klocworkServerUrl}/${klocworkProject}" --tables-directory "${tablesDirectory}" --force "${buildSpecsDirectory}/mtwilson.out" "${buildSpecsDirectory}/aikqverify.out" "${buildSpecsDirectory}/aikqverify_services.out" "${buildSpecsDirectory}/hex2bin_trustagent.out" "${buildSpecsDirectory}/aikquote_trustagent.out" "${buildSpecsDirectory}/niarl_plain.out" "${buildSpecsDirectory}/niarl_sha1.out" "${buildSpecsDirectory}/tpm_tools.out" "${buildSpecsDirectory}/hex2bin.out" "${buildSpecsDirectory}/tpm_createkey.out" "${buildSpecsDirectory}/tpm_cit_commands.out"
+  kwbuildproject --url "${klocworkServerUrl}/${klocworkProject}" --tables-directory "${tablesDirectory}" --force "${buildSpecsDirectory}/mtwilson.out" "${buildSpecsDirectory}/aikqverify.out" "${buildSpecsDirectory}/aikqverify_services.out" "${buildSpecsDirectory}/hex2bin_trustagent.out" "${buildSpecsDirectory}/aikquote_trustagent.out" "${buildSpecsDirectory}/niarl_plain.out" "${buildSpecsDirectory}/niarl_sha1.out" "${buildSpecsDirectory}/hex2bin.out" "${buildSpecsDirectory}/tpm_createkey.out" "${buildSpecsDirectory}/tpm_cit_commands.out"
 }
 
 uploadResults() {
