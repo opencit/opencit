13b8e322eee6d4a64f970b57c266e1d3085201d8
Haidong Xia <haidong.xia@intel.com>
2016-08-15 18:31:16 -0700

    dev-cit-2.2: fix Bug #5604:  redhat-lsb-core package and if not found download it using yum for RHEL

--- installers/mtwilson-trustagent-rhel/src/files/setup.sh
+++ installers/mtwilson-trustagent-rhel/src/files/setup.sh
@@ -93,7 +93,7 @@ if [ -f version ]; then . version; else echo_warning "Missing file: version"; fi
 
 # make sure unzip and authbind are installed
 #java_required_version=1.7.0_51
-TRUSTAGENT_YUM_PACKAGES="zip unzip authbind openssl tpm-tools make gcc trousers trousers-devel"
+TRUSTAGENT_YUM_PACKAGES="zip unzip authbind openssl tpm-tools make gcc trousers trousers-devel redhat-lsb-core"
 TRUSTAGENT_APT_PACKAGES="zip unzip authbind openssl libssl-dev libtspi-dev libtspi1 make gcc trousers trousers-dbg"
 TRUSTAGENT_YAST_PACKAGES="zip unzip authbind openssl libopenssl-devel tpm-tools make gcc trousers trousers-devel"
 TRUSTAGENT_ZYPPER_PACKAGES="zip unzip authbind openssl libopenssl-devel libopenssl1_0_0 openssl-certs trousers trousers-devel"
@@ -109,7 +109,7 @@ else
   #./mtwilson-tpm2-packages-2.2-SNAPSHOT.bin
 
   # not install trousers and its dev packages for tpm 2.0
-  TRUSTAGENT_YUM_PACKAGES="zip unzip authbind openssl make gcc"
+  TRUSTAGENT_YUM_PACKAGES="zip unzip authbind openssl make gcc redhat-lsb-core"
   TRUSTAGENT_APT_PACKAGES="zip unzip authbind openssl libssl-dev make gcc binutils"
   TRUSTAGENT_YAST_PACKAGES="zip unzip authbind openssl libopenssl-devel make gcc"
   TRUSTAGENT_ZYPPER_PACKAGES="zip unzip authbind openssl libopenssl-devel libopenssl1_0_0 openssl-certs"
