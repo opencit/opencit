9c024705c14d4f672152c4c8e8515fb18b4a4bf5
Ryan Savino <ryan.k.savino@intel.com>
2016-08-18 15:54:09 -0700

    v2.1.0+feature#5600-bkc-tool: formatting

--- packages/cit-bkc-tool/src/main/linux/cit-bkc-validation.sh
+++ packages/cit-bkc-tool/src/main/linux/cit-bkc-validation.sh
@@ -18,22 +18,22 @@ test_tpm_support() {
   if [ "$file" = "/dev/tpm0" ]; then
     echo "TPM is supported."
   else
-    echo "TPM is not supported."      
-    echo "TPM is not supported." >> $LOG_FILE      
-  fi  
+    echo "TPM is not supported."
+    echo "TPM is not supported." >> $LOG_FILE
+  fi
 }
 
 test_tpm_ownership() {
   if [[ "$(cat /sys/class/misc/tpm0/device/owned)" == 1 ]]; then
     echo "TPM is owned."
   else
-    echo "TPM is not owned."      
-    echo "TPM is not owned." >> $LOG_FILE      
+    echo "TPM is not owned."
+    echo "TPM is not owned." >> $LOG_FILE
   fi
 }
 
 test_txt_support() {
-  TXT=$(cat /proc/cpuinfo | grep -o "smx" | head -1) 
+  TXT=$(cat /proc/cpuinfo | grep -o "smx" | head -1)
   if [ $TXT = "smx" ]; then
     echo "TXT is supported."
   else
@@ -49,7 +49,7 @@ test_aik_present() {
   else
     echo "AIK certificate ($AIKCertFile) does not exist."
     echo "AIK certificate ($AIKCertFile) does not exist." >> $LOG_FILE
-  fi  
+  fi
 }
 
 test_bindingkey_present() {
@@ -59,9 +59,9 @@ test_bindingkey_present() {
   else
     echo "Binding key certificate ($BindingKeyFile) does not exist."
     echo "Binding key certificate ($BindingKeyFile) does not exist." >> $LOG_FILE
-  fi  
+  fi
 }
-  
+
 test_signingkey_present() {
   SigningKeyFile="/opt/trustagent/configuration/signingkey.pem"
   if [ -f $SigningKeyFile ]; then
@@ -69,7 +69,7 @@ test_signingkey_present() {
   else
     echo "Signing key certificate ($SigningKeyFile) does not exist."
     echo "Signing key certificate ($SigningKeyFile) does not exist." >> $LOG_FILE
-  fi  
+  fi
 }
 
 test_nvindex_defined() {
@@ -79,16 +79,16 @@ test_nvindex_defined() {
   else
     echo "NV index not defined. Asset tags cannot be configured."
     echo "NV index not defined. Asset tags cannot be configured." >> $LOG_FILE
-  fi  
+  fi
 }
 
 test_create_whitelist() {
   
 }
 
-#test_register_host() {  }
+#test_register_host() {}
 
-#test_write_assettag() {  }
+#test_write_assettag() {}
 
 #test_get_pcr0() {}
 
