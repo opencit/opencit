912ca8eab39a39e4f985326575ff5f28dc380bae
Ryan Savino <ryan.k.savino@intel.com>
2016-08-23 18:03:18 -0700

    v2.1.0+feature#5600-bkc-tool: added header for accepting xml content back from server

--- packages/cit-bkc-tool/src/main/linux/cit-bkc-validation.sh
+++ packages/cit-bkc-tool/src/main/linux/cit-bkc-validation.sh
@@ -183,6 +183,7 @@ test_write_assettag() {
   bkc_test_name="write_assettag"
   
   curl --noproxy 127.0.0.1 -k -vs \
+    -H "Content-Type: application/xml" \
     https://127.0.0.1:8443/mtwilson/v2/hosts?nameEqualTo=127.0.0.1 \
     1>$CIT_BKC_DATA_PATH/$assettag_data_file 2>$CIT_BKC_DATA_PATH/$assettag_http_status_file
   
