186667133e242f11098e8280a3eac09af526f3a9
Alfy26 <martinoalfonso92@gmail.com>
2017-05-15 14:28:56 +0200

    Revert "Added new function in BuildQuoteXMLCmd.java to insert inside Quote th…"

--- trust-agent/mtwilson-trustagent-ws-v2/src/main/java/com/intel/mountwilson/trustagent/commands/BuildQuoteXMLCmd.java
+++ trust-agent/mtwilson-trustagent-ws-v2/src/main/java/com/intel/mountwilson/trustagent/commands/BuildQuoteXMLCmd.java
@@ -38,8 +38,6 @@ public class BuildQuoteXMLCmd implements ICommand {
             tpmQuoteResponse.eventLog = context.getModules(); //base64-encoded  xml which the caller will interpret
             tpmQuoteResponse.tcbMeasurement = context.getTcbMeasurement();
             tpmQuoteResponse.selectedPcrBanks = context.getSelectedPcrBanks();
-            //Adding new function to insert inside Quote the IMA measurements
-            tpmQuoteResponse.SimpleSnapshotObject.addAll(readValuesFromIma());
             context.setTpmQuoteResponse(tpmQuoteResponse);
         }
         catch(Exception e) {
@@ -60,59 +58,5 @@ public class BuildQuoteXMLCmd implements ICommand {
         log.debug("Final content that is being sent back to the AS is : " + responseXML);
             context.setResponseXML(responseXML);
 */
-    }
-    
-        public ArrayList<Objects> readValuesFromIma () {
-    	String[] ima_parts = new String[5];
-    	String[] parts = new String[2];
-    	ArrayList<Objects> list = new ArrayList<>();
-		String s;
-		try {
-            //Opening IMA runtime measurement list
-			FileReader ima = new FileReader("/sys/kernel/security/ima/ascii_runtime_measurements");
-			BufferedReader row = new BufferedReader(ima);
-			while (true) {
-				s = row.readLine();
-				if (s==null)
-					break;
-				if (!s.startsWith(defaultPCRnumber))
-					continue;
-
-				ima_parts=s.split(" ");
-				Objects value = new Objects();
-				value.setHash(ima_parts[template_hash]);
-				value.setPcrindex(defaultPCRnumber);
-				value.setEventtype(defaultEventType);
-                //Managing both types of IMA templates (ima and ima-ng)
-				if (ima_parts[3].startsWith("sha1") || ima_parts[3].startsWith("sha256")) {
-					String filedata_hash[] = new String[2];
-					filedata_hash = ima_parts[3].split(":");
-					byte[] data_value = filedata_hash[1].getBytes("UTF-8");
-					value.setEventdigest(Base64.encodeBase64String(data_value));
-				} else {
-					byte[] data_value = ima_parts[filedata_hash].getBytes("UTF-8");
-					value.setEventdigest(Base64.encodeBase64String(data_value));
-				}
-				byte[] data_name = ima_parts[filename_hint].getBytes("UTF-8");
-				value.setEventdata(Base64.encodeBase64String(data_name));
-
-				list.add(value);
-			}
-		
-			row.close();
-			ima.close();
-			return list;
-			
-		} catch (IOException e) {
-            //If IMA is not available the list will contain only the item shown below
-			Objects value = new Objects();
-			value.setPcrindex(defaultPCRnumber);
-			value.setHash("not available");
-			value.setEventtype(defaultEventType);
-			value.setEventdata("not available");
-			value.setEventdigest("not available");
-			list.add(value);
-    	return list;
-		}	
     }
 }
